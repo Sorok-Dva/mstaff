f9aa5efa3ffb130787f0bdab34986e28442a34b3
Sorok-Dva <garutor@gmail.com>
2018-12-13 18:01:31 +0100

    add image routes and controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,7 @@ const indexController = {};
 
 indexController.getIndex = (req, res) => res.render('index', { layout: 'landing' });
 indexController.getLogin = (req, res) => res.render('users/login');
+indexController.getRegister = (req, res) => res.render('users/register');
+indexController.postRegister = (req, res) => res.render('users/register');
 
 module.exports = indexController;
