b022b9530f18c5d02eccbbf8f4619733acc0285b
Sorok-Dva <garutor@gmail.com>
2018-12-13 23:30:06 +0100

    Login part

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,8 @@ const indexController = {};
 
 indexController.getIndex = (req, res) => res.render('index', { layout: 'landing' });
 indexController.getLogin = (req, res) => res.render('users/login');
+indexController.postLogin = (req, res) => res.redirect('/');
+indexController.getLogout = (req, res) => req.logout() + res.redirect('/');
 indexController.getRegister = (req, res) => res.render('users/register');
 
 module.exports = indexController;
--- controllers/user.js
+++ controllers/user.js
@@ -12,6 +12,15 @@ UserController.ensureIsNotAuthenticated = (req, res, next) => {
   }
 };
 
+UserController.ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.flash('error_msg', 'You are not logged');
+    res.redirect('/');
+  }
+};
+
 UserController.create = (req, res) => {
   let password = req.body.password;
   bcrypt.genSalt(10).then(salt => {
@@ -21,9 +30,16 @@ UserController.create = (req, res) => {
         lastName: req.body.lastName,
         email: req.body.email,
         password: hash
-      }).then(user => res.render('login', {user}))
+      }).then(user => res.render('login', { user }))
         .catch(error => res.render('users/register', { sequelizeError: error }));
     });
+  });
+};
+
+UserController.comparePassword = (candidatePassword, hash, callback) => {
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if (err) throw err;
+    callback(null, isMatch);
   });
 };
 
