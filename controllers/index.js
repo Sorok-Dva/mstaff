8e6c41b7bfda1e43a9b3f37ace75eaf6d2ff1b3b
Sorok-Dva <garutor@gmail.com>
2018-12-13 02:35:08 +0100

    assets + landing integration

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const indexController = {};
 
-indexController.getIndex = (req, res) => res.render('index', {title: req.i18n_texts.title});
-indexController.getLogin = (req, res) => res.render('users/login', {title: req.i18n_texts.title});
+indexController.getIndex = (req, res) => res.render('index', {layout: 'landing'});
+indexController.getLogin = (req, res) => res.render('users/login');
 
 module.exports = indexController;
