de4eda3d354781574ba281e106d3d4b1785658e3
Sorok-Dva <garutor@gmail.com>
2018-12-14 14:13:02 +0100

    edit app

--- app.js
+++ app.js
@@ -106,7 +106,6 @@ app.use((req, res, next) => {
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
   res.locals.user = req.user || null;
-  res.locals.session = req.user || null;
   next();
 });
 
