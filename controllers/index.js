8c4395a45aa7fcd6f50d1f8ebb20555c9b168ef0
Sorok-Dva <garutor@gmail.com>
2018-12-14 14:59:40 +0100

    controllers refactoring

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,7 @@
-const indexController = {};
-
-indexController.getIndex = (req, res) => res.render('index', { layout: 'landing' });
-indexController.getLogin = (req, res) => res.render('users/login');
-indexController.postLogin = (req, res) => res.redirect('/');
-indexController.getLogout = (req, res) => req.logout() + res.redirect('/');
-indexController.getRegister = (req, res) => res.render('users/register');
-
-module.exports = indexController;
+module.exports = {
+  getIndex:     (req, res) => res.render('index', { layout: 'landing' }),
+  getLogin:     (req, res) => res.render('users/login'),
+  postLogin:    (req, res) => res.redirect('/'),
+  getLogout:    (req, res) => req.logout() + res.redirect('/'),
+  getRegister:  (req, res) => res.render('users/register')
+};
--- controllers/user.js
+++ controllers/user.js
@@ -1,46 +1,43 @@
+const User = require('../models/index').User;
 const bcrypt = require('bcryptjs');
 
-const User = require('../models/index').User;
-const UserController = {};
-
-UserController.ensureIsNotAuthenticated = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    return next();
-  } else {
-    req.flash('error_msg', 'Vous êtes déjà connecté.');
-    res.redirect('/');
-  }
-};
-
-UserController.ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    req.flash('error_msg', 'You are not logged');
-    res.redirect('/');
-  }
-};
-
-UserController.create = (req, res) => {
-  let password = req.body.password;
-  bcrypt.genSalt(10).then(salt => {
-    bcrypt.hash(password, salt).then(hash => {
-      User.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: hash
-      }).then(user => res.render('login', { user }))
-        .catch(error => res.render('users/register', { sequelizeError: error }));
+module.exports = {
+  // Middlewares
+  ensureIsNotAuthenticated: (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      return next();
+    } else {
+      req.flash('error_msg', 'Vous êtes déjà connecté.');
+      res.redirect('/');
+    }
+  },
+  ensureAuthenticated: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      req.flash('error_msg', 'You are not logged');
+      res.redirect('/');
+    }
+  },
+  // End Middlewares
+  create: (req, res) => {
+    let password = req.body.password;
+    bcrypt.genSalt(10).then(salt => {
+      bcrypt.hash(password, salt).then(hash => {
+        User.create({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: hash
+        }).then(user => res.render('login', { user }))
+          .catch(error => res.render('users/register', { sequelizeError: error }));
+      });
     });
-  });
+  },
+  comparePassword: (candidatePassword, hash, callback) => {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+      if (err) throw err;
+      callback(null, isMatch);
+    });
+  }
 };
-
-UserController.comparePassword = (candidatePassword, hash, callback) => {
-  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-    if (err) throw err;
-    callback(null, isMatch);
-  });
-};
-
-module.exports = UserController;
