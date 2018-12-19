af6018a0d42e21b8c3da20098b8fcc2fcdcccfed
Sorok-Dva <garutor@gmail.com>
2018-12-19 18:36:32 +0100

    refactoring app.js, put all middlewares in one different file

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-const express = require('express');
-const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
-const exphbs = require('express-handlebars');
-const fileUpload = require('express-fileupload');
-const compress = require('compression');
-const cors = require('cors');
-const path = require('path');
-const csurf = require('csurf');
-const cookieParser = require('cookie-parser');
-const handlebars = require('./helpers/index').register(require('handlebars'));
-const flash = require('connect-flash');
-const passport = require('passport');
-const helmet = require('helmet');
-const i18n = require('i18n-express');
 const conf = require('dotenv').config().parsed;
-const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
+
+const express = require('express');
+const middleware = require('./middlewares');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -28,101 +15,34 @@ const app = express();
 const env = conf.ENV || 'development';
 const config = require(`${__dirname}/config/config.json`)[env];
 
-// -- Express Session configuration
-let sessionStore = new MySQLStore({
-  host: config.host,
-  user: config.username,
-  password: config.password,
-  database: config.database
-});
-
+// express config
 app.set('env', env);
 app.set('trust proxy', true);
-// secure apps by setting various HTTP headers
-app.use(helmet());
-// parse body params and attache them to req.body
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser(conf.SECRET));
-// enable crsf token middleware
-app.use(csurf({ cookie: true }));
-
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors());
-app.use(session({
-  secret: conf.SECRET,
-  saveUninitialized: true,
-  store: sessionStore,
-  resave: true
-}));
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-if (!env === 'development') {
+if (env !== 'development') {
   app.set('view cache', true);
-  console.log('view cache');
 }
-
 app.set('view engine', 'hbs');
 
-// express config
-app.engine('hbs', exphbs({
-  extname         : 'hbs',
-  defaultLayout   : 'default',
-  layoutsDir      : path.join(__dirname, '/views/layouts'),
-  partialsDir     : path.join(__dirname, '/views/partials')
-}));
-app.use(i18n({
-  translationsPath: path.join(__dirname, 'i18n'),
-  cookieLangName: 'mstaff_lang',
-  browserEnable: true,
-  defaultLang: 'fr',
-  siteLangs: ['fr'],
-  textsVarName: 'tr'
-}));
+// ------ MIDDLEWARES
+app.engine('hbs', middleware.exphbs);
 
-app.use(compress({
-  filter: function (req, res) {
-    return (/json|text|javascript|css|image\/svg\+xml|application\/x-font-ttf/).test(res.getHeader('Content-Type'));
-  },
-  level: 9
-}));
-
-if (env === 'development' || env === 'local') {
-  app.use(logger('dev'));
-  // error handling code within middleware
-  process.on('uncaughtException', error => {
-    console.log('uncaught');
-    if(!error.isOperational) {
-      throw new Error(error);
-    }
-    console.log('process exit');
-    process.exit(1);
-  });
-}
-
+app.use(middleware.helmet);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(middleware.cookieParser);
+app.use(middleware.csurf);
+app.use(middleware.session);
+app.use(middleware.sass);
+app.use(middleware.i18n);
+app.use(middleware.compress);
 app.use('/static', express.static(path.join(__dirname, 'public')));
-
-app.use(fileUpload());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use((req, res, next) => {
-  if (req.url.search('static') !== -1) return next();
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
+app.use(middleware.fileUpload);
+app.use(middleware.passportInit);
+app.use(middleware.passportSession);
+app.use(middleware.flash);
+app.use(middleware.setLocals);
 
 // ------ ROUTES
 app.use('/', indexRouter);
@@ -131,16 +51,17 @@ app.use('/back-office', boRouter);
 app.use('/api', apiRouter);
 app.use('/api/user', apiUserRouter);
 
-// error handler
-app.use((err, req, res, next) => {
-  console.log(err);
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = env === 'development' ? err : {};
+if (env === 'development' || env === 'local') {
+  app.use(middleware.loggerDev);
+  process.on('uncaughtException', error => {
+    console.log('uncaught');
+    if(!error.isOperational) {
+      throw new Error(error);
+    }
+    process.exit(1);
+  });
+}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ middlewares.js
@@ -0,0 +1,89 @@
+const conf = require('dotenv').config().parsed;
+const path = require('path');
+const env = 'development';
+const config = require(`./config/config.json`)[env];
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const exphbs = require('express-handlebars');
+const fileUpload = require('express-fileupload');
+const compress = require('compression');
+const cors = require('cors');
+const csurf = require('csurf');
+const cookieParser = require('cookie-parser');
+const handlebars = require('./helpers/index').register(require('handlebars'));
+const flash = require('connect-flash');
+const passport = require('passport');
+const helmet = require('helmet');
+const i18n = require('i18n-express');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+
+let sessionStore = new MySQLStore({
+  host: config.host,
+  user: config.username,
+  password: config.password,
+  database: config.database
+});
+
+module.exports = {
+  compress: compress({
+    filter: (req, res) => {
+      return (/json|text|javascript|css|image\/svg\+xml|application\/x-font-ttf/).test(res.getHeader('Content-Type'));
+    },
+    level: 9
+  }),
+  cookieParser: cookieParser(conf.SECRET),
+  cors: cors(), // enable CORS - Cross Origin Resource Sharing
+  csurf: csurf({ cookie: true }), // enable crsf token middleware
+  errorHandler: (err, req, res, next) => { // error handler
+    console.log(err);
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = env === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  },
+  exphbs: exphbs({
+    extname         : 'hbs',
+    defaultLayout   : 'default',
+    layoutsDir      : path.join(__dirname, '/views/layouts'),
+    partialsDir     : path.join(__dirname, '/views/partials')
+  }),
+  fileUpload: fileUpload(),
+  flash: flash(),
+  helmet: helmet(), // secure apps by setting various HTTP headers
+  i18n: i18n({
+    translationsPath: path.join(__dirname, 'i18n'),
+    cookieLangName: 'mstaff_lang',
+    browserEnable: true,
+    defaultLang: 'fr',
+    siteLangs: ['fr'],
+    textsVarName: 'tr'
+  }),
+  loggerDev: logger('dev'),
+  passportInit: passport.initialize,
+  passportSession: passport.session,
+  sass: sassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: false, // true = .sass and false = .scss
+    sourceMap: true
+  }),
+  setLocals: (req, res, next) => {
+    if (req.url.search('static') !== -1) return next();
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+  },
+  session: session({
+    secret: conf.SECRET,
+    saveUninitialized: true,
+    store: sessionStore,
+    resave: true
+  })
+};
