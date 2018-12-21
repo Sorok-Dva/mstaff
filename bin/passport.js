38987d436630bdd1879049183cb6c9d294108eac
Sorok-Dva <garutor@gmail.com>
2018-12-21 16:21:01 +0100

    refacto passport bin

--- bin/passport.js
+++ bin/passport.js
@@ -4,16 +4,12 @@ const LocalStrategy = require('passport-local').Strategy;
 const Models = require('../models');
 const UserController = require('../controllers/user');
 
-let attributes = ['id', 'password', 'email', 'type', 'role'];
+let attributes = ['id', 'password', 'email', 'firstName', 'lastName', 'type', 'role'];
 
 passport.use(new LocalStrategy((email, password, done) => {
   Models.User.findOne({
     where: { email },
-    attributes,
-    include:[{
-      model: Models.Candidate,
-      as: 'candidate'
-    }]
+    attributes
   }).then(user => {
     if (!user) return done(null, false, { message: 'Utilisateur inconnu' });
     UserController.comparePassword(password, user.dataValues.password, (err, isMatch) => {
@@ -22,7 +18,6 @@ passport.use(new LocalStrategy((email, password, done) => {
         let session = {
           id: user.dataValues.id,
           email: user.dataValues.email,
-          type: user.dataValues.type,
           role: user.dataValues.role
         };
         return done(null, session);
@@ -37,7 +32,17 @@ passport.use(new LocalStrategy((email, password, done) => {
 
 passport.serializeUser((user, done) => done(null, user.id));
 
-passport.deserializeUser((id, done) =>
-  Models.User.findOne({ attributes, where: { id } }).then(user => done(null, user)));
+passport.deserializeUser((id, done) => {
+  Models.User.findOne({ attributes, where: { id } }).then(user => {
+    let session = {
+      id: user.dataValues.id,
+      email: user.dataValues.email,
+      identity: `${user.dataValues.firstName} ${user.dataValues.lastName}`,
+      type: user.dataValues.type,
+      role: user.dataValues.role
+    };
+    done(null, session)
+  });
+});
 
 module.exports = passport;
