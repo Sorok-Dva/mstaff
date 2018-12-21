eafa5d03d0ac7ddf7fbbd9d59ecb55f57feb9a6f
Sorok-Dva <garutor@gmail.com>
2018-12-21 18:00:04 +0100

    add getProfile method (User.findOne including : candidates, experience, service and post models relation

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
-const Establishment = require('../models/index').Establishment;
+const Models = require('../models/index');
 
 module.exports = {
-  getIndex:     (req, res) => {
+  getIndex:     (req, res, next) => {
     // If the url call an es (ex: sante.mstaff.co) so we show es homepage else redirect to mstaff.co
     if (req.headers.host.split('.')[0] !== req.headers.host) {
-      Establishment.findOne({
+      Models.Establishment.findOne({
         where: {
           domain_enable: true,
           domain_name: req.headers.host.split('.')[0]
@@ -14,17 +14,18 @@ module.exports = {
         return res.render('establishments/homepage');
       });
     } else {
-      let opts = {};
-      if (!req.user) opts.layout = 'landing';
-      return res.render('index', opts)
+      if (req.user) {
+        return res.redirect('/profile')
+      }
+      return res.render('index', { layout: 'landing' })
     }
   },
-  getLogin:     (req, res) => res.render('users/login'),
+  getLogin:     (req, res) => res.render('users/login', { layout: 'onepage' }),
   postLogin:    (req, res) => res.redirect('/'),
   getLogout:    (req, res) => req.logout() + res.redirect('/'),
   getRegister:  (req, res) => {
     if (req.params.esCode) {
-      Establishment.findOne({
+      Models.Establishment.findOne({
         attributes: ['id', 'name', 'code'],
         where: {
           code: req.params.esCode
@@ -35,14 +36,51 @@ module.exports = {
           return res.redirect('/register');
         } else {
           es = es.dataValues;
-          res.render('users/register', { es });
+          res.render('users/register', { layout: 'onepage', es });
         }
       });
     } else {
-      res.render('users/register');
+      res.render('users/register', { layout: 'onepage' });
     }
   },
-  getRegisterWizard:  (req, res) => console.log(res.locals) + res.render('users/registerWizard'),
-  get404:       (req, res) => res.render('error', { error: 'Lien invalide' }),
-  getRegisterDemo:(req, res) => res.render('demo/register')
+  getRegisterWizard:  (req, res) => res.render('users/registerWizard'),
+  get404: (req, res) => res.render('error', { error: 'Lien invalide' }),
+  getRegisterDemo:(req, res) => res.render('demo/register'),
+  getProfile: (req, res, next) => {
+    if (req.user.type === 'candidate') {
+      Models.User.findOne({
+        where: { id: req.user.id },
+        include:[{
+          model: Models.Candidate,
+          as: 'candidate',
+          include: {
+            model: Models.Experience,
+            as: 'experiences',
+            include: [{
+              model: Models.Service,
+              as: 'service'
+            }, {
+              model: Models.Post,
+              as: 'poste'
+            }]
+          }
+        }]
+      }).then(user => {
+        return res.render('users/profile', { user, a: { main: 'profile' } })
+      }).catch(error => next(error));
+    }
+  },
+  getEditProfile: (req, res, next) => {
+    if (req.user.type === 'candidate') {
+      Models.User.findOne({
+        where: { id: req.user.id },
+        include:[{
+          model: Models.Candidate,
+          as: 'candidate'
+        }]
+      }).then(user => {
+        return res.render('users/edit', { user, a: { main: 'profile' } })
+      }).catch(error => next(error));
+    }
+  }
 };
