79f1411f1edfc26a23ebf6415b21ec5fcd30883c
Sorok-Dva <garutor@gmail.com>
2019-01-02 17:22:13 +0100

    logs some info to make subdomain working

--- controllers/index.js
+++ controllers/index.js
@@ -3,22 +3,27 @@ const Models = require('../models/index');
 module.exports = {
   getIndex:     (req, res, next) => {
     // If the url call an es (ex: sante.mstaff.co) so we show es homepage else redirect to mstaff.co
-    if (req.headers.host.split('.')[0] !== req.headers.host) {
-      Models.Establishment.findOne({
-        where: {
-          domain_enable: true,
-          domain_name: req.headers.host.split('.')[0]
+    if (req.headers.host === 'localhost' || '127.0.0.1') {
+      if (req.headers.host.split('.')[0] !== req.headers.host) {
+        Models.Establishment.findOne({
+          where: {
+            domain_enable: true,
+            domain_name: req.headers.host.split('.')[0]
+          }
+        }).then(es => {
+          if (!es) return res.redirect(`${req.headers.host.split('.')[1]}/404`);
+          return res.render('establishments/homepage');
+        });
+      } else {
+        if (req.user && (req.user.type === 'candidate' || 'admin')) {
+          return res.redirect('/profile')
         }
-      }).then(es => {
-        if (!es) return res.redirect(`${req.headers.host.split('.')[1]}/404`);
-        return res.render('establishments/homepage');
-      });
-    } else {
-      if (req.user && (req.user.type === 'candidate' || 'admin')) {
-        return res.redirect('/profile')
+        return res.render('index', { layout: 'landing' })
       }
-      return res.render('index', { layout: 'landing' })
+    } else {
+      console.log(req.headers.host.split('.'), req.headers.host);
     }
+
   },
   getLogin:     (req, res) => res.render('users/login', { layout: 'onepage' }),
   postLogin:    (req, res) => res.redirect('/'),
