1393cfbe35e889333cabe3b46aed74033f1ba26f
Sorok-Dva <garutor@gmail.com>
2019-01-04 16:47:01 +0100

    add some routes

--- routes/api/candidate.js
+++ routes/api/candidate.js
@@ -1,11 +1,13 @@
-/*
 const express = require('express');
 const router = express.Router();
 
+const UserController = require('../../controllers/user');
 const CandidateController = require('../../controllers/candidate');
 
-router.get('/emailAvailable/:email',
-  UserController.validate('ApiVerifyEmailAvailability'),
-  UserController.ApiVerifyEmailAvailability);
-
-module.exports = router; */
+router.get('/xp/:id', UserController.ensureIsCandidate, CandidateController.getXpById)
+  .delete('/xp/:id', UserController.ensureIsCandidate, CandidateController.removeXP);
+router.get('/formation/:id', UserController.ensureIsCandidate, CandidateController.getFormationById)
+  .delete('/formation/:id', UserController.ensureIsCandidate, CandidateController.removeFormation);
+router.get('/diploma/:id', UserController.ensureIsCandidate, CandidateController.getDiplomaById)
+  .delete('/diploma/:id', UserController.ensureIsCandidate, CandidateController.removeDiploma);
+module.exports = router;
