0003a2dbaeb6d1fe0c39cd92f449b2f07392f5da
Сорок два <garcia.llyam@gmail.com>
2019-01-08 16:11:12 +0100

    skills refactoring (239 -> 137 lines of code)

--- public/javascripts/skills.js
+++ public/javascripts/skills.js
@@ -1,209 +1,96 @@
-let listSkills = [];
-let listEquipments = [];
-let listSoftwares = [];
+let toAutocomplete = ['skills', 'equipments', 'softwares'];
 
-$.get('/api/skills/all', (data) => $.map(data.skills, skill => listSkills.push(skill.name)));
-$.get('/api/equipments/all', (data) => $.map(data.equipments, equipment => listEquipments.push(equipment.name)));
-$.get('/api/softwares/all', (data) => $.map(data.softwares, software => listSoftwares.push(software.name)));
+toAutocomplete.forEach((ac) => {
+  let list = [];
+  let e = ac.substring(0, ac.length - 1);
+  let eCapitalize = e.charAt(0).toUpperCase() + e.slice(1);
+  console.log(e, eCapitalize);
 
-$('#skills').autocomplete({
-  source: listSkills,
-  minLength: 3,
-  select: (event, ui) => {
-    if ($(`tr[data-type="skill"]>td:contains('${ui.item.label}')`).length > 0) {
-      $('#addNewSkill').attr('disabled', 'disabled');
-      $('#skillAlreadyInList').show();
-    } else {
-      $('#addNewSkill').removeAttr('disabled');
-      $('#skillAlreadyInList').hide();
-    }
-  },
-  search: (event, ui) => {
-    if ($(`tr[data-type="skill"]>td:contains('${event.target.value}')`).length > 0) {
-      $('#addNewSkill').attr('disabled', 'disabled');
-      $('#skillAlreadyInList').show();
-    } else {
-      $('#addNewSkill').removeAttr('disabled');
-      $('#skillAlreadyInList').hide();
-    }
-  }
-});
+  $.get(`/api/${ac}/all`, (data) => $.map(data[`${ac}`], value => list.push(value.name)));
 
-$('#equipments').autocomplete({
-  source: listEquipments,
-  minLength: 3,
-  select: (event, ui) => {
-    if ($(`tr[data-type="equipment"]>td:contains('${ui.item.label}')`).length > 0) {
-      $('#addNewEquipment').attr('disabled', 'disabled');
-      $('#equipmentAlreadyInList').show();
-    } else {
-      $('#addNewEquipment').removeAttr('disabled');
-      $('#equipmentAlreadyInList').hide()
+  $(`#${ac}`).autocomplete({
+    source: list,
+    minLength: 3,
+    select: (event, ui) => {
+      autocompleteTrigger({
+        type: e, add: `addNew${eCapitalize}`, already: `${e}AlreadyInList`
+      }, ui.item.label)
+    },
+    search: (event, ui) => {
+      autocompleteTrigger({
+        type: e, add: `addNew${eCapitalize}`, already: `${e}AlreadyInList`
+      }, event.target.value)
     }
-  },
-  search: (event, ui) => {
-    if ($(`tr[data-type="equipment"]>td:contains('${event.target.value}')`).length > 0) {
-      $('#addNewEquipment').attr('disabled', 'disabled');
-      $('#equipmentAlreadyInList').show();
-    } else {
-      $('#addNewEquipment').removeAttr('disabled');
-      $('#equipmentAlreadyInList').hide();
-    }
-  }
-});
-$('#softwares').autocomplete({
-  source: listSoftwares,
-  minLength: 3,
-  select: (event, ui) => {
-    if ($(`tr[data-type="software"]>td:contains('${ui.item.label}')`).length > 0) {
-      $('#addNewSoftware').attr('disabled', 'disabled');
-      $('#softwareAlreadyInList').show();
-    } else {
-      $('#addNewSoftware').removeAttr('disabled');
-      $('#softwareAlreadyInList').hide()
-    }
-  },
-  search: (event, ui) => {
-    if ($(`tr[data-type="software"]>td:contains('${event.target.value}')`).length > 0) {
-      $('#addNewSoftware').attr('disabled', 'disabled');
-      $('#softwareAlreadyInList').show();
-    } else {
-      $('#addNewSoftware').removeAttr('disabled');
-      $('#softwareAlreadyInList').hide();
-    }
-  }
-});
-
-$('#addNewSkill').click(() => {
-  let _csrf = $('#csrfToken').val();
-  let name = $('#skills').val();
-  $.post('/api/candidate/skills/add', {name, _csrf}, (data) => {
-    if (data.skill) {
-      $('#skills').val('');
-      notification({
-        icon: 'check-circle',
-        type: 'success',
-        title: 'Compétence ajoutée avec succès :',
-        message: `Votre compétence "${data.skill.name}" vient d'être ajoutée à votre liste.`
-      });
-      $("#skillsList").append($('<tr>')
-        .attr({'data-type': 'skill', 'data-id': data.skill.id})
-        .append($('<td>').text(`${data.skill.name}`))
-        .append($('<td>').append($('<div>').attr({class: 'rating-stars text-center'})
-          .append($('<ul>').attr({
-            class: 'stars',
-            'data-type': 'skill',
-            'data-id': data.skill.id
-          })
-            .append($('<li>').attr({class: 'star', title: 'Fair', 'data-value': 1}).html('<i class="fa fa-star"></i>'))
-            .append($('<li>').attr({class: 'star', title: 'Good', 'data-value': 2}).html('<i class="fa fa-star"></i>'))
-            .append($('<li>').attr({class: 'star', title: 'Excellent', 'data-value': 3}).html('<i class="fa fa-star"></i>')))
-        ))
-        .append($('<td>').append($('<button>').attr({
-          class: 'btn btn-simple btn-danger btn-icon remove',
-          'data-type': 'skill',
-          'data-id': data.skill.id
-        }).html('<i class="fa fa-trash"></i>')))
-      );
-    }
-  }).catch(error => {
-    error = error.responseJSON;
-    if (error.error === 'Already exists') {
-      notification({
-        icon: 'exclamation',
-        type: 'danger',
-        title: 'Ajout impossible',
-        message: `Cette compétence est déjà ajoutée à votre profil.`
-      });
-    } else return errorsHandler(error);
   });
-});
-$('#addNewEquipment').click(() => {
-  let _csrf = $('#csrfToken').val();
-  let name = $('#equipments').val();
-  $.post('/api/candidate/equipments/add', {name, _csrf}, (data) => {
-    if (data.equipment) {
-      $('#equipments').val('');
-      notification({
-        icon: 'check-circle',
-        type: 'success',
-        title: 'Matériel / produit ajouté avec succès :',
-        message: `Votre matériel ou produit "${data.equipment.name}" vient d'être ajouté à vos compétences.`
-      });
-      $("#equipmentsList").append($('<tr>')
-        .attr({'data-type': 'equipment', 'data-id': data.equipment.id})
-        .append($('<td>').text(`${data.equipment.name}`))
-        .append($('<td>').append($('<div>').attr({class: 'rating-stars text-center'})
-          .append($('<ul>').attr({
-            class: 'stars',
-            'data-type': 'equipment',
-            'data-id': data.equipment.id
-          })
-            .append($('<li>').attr({class: 'star', title: 'Fair', 'data-value': 1}).html('<i class="fa fa-star"></i>'))
-            .append($('<li>').attr({class: 'star', title: 'Good', 'data-value': 2}).html('<i class="fa fa-star"></i>'))
-            .append($('<li>').attr({class: 'star', title: 'Excellent', 'data-value': 3}).html('<i class="fa fa-star"></i>')))
-        ))
-        .append($('<td>').append($('<button>').attr({
-          class: 'btn btn-simple btn-danger btn-icon remove',
-          'data-type': 'equipment',
-          'data-id': data.equipment.id
-        }).html('<i class="fa fa-trash"></i>')))
-      );
-    }
-  }).catch(error => {
-    error = error.responseJSON;
-    if (error.error === 'Already exists') {
-      notification({
-        icon: 'exclamation',
-        type: 'danger',
-        title: 'Ajout impossible',
-        message: `Ce matériel ou produit est déjà ajouté à votre profil.`
-      });
-    } else return errorsHandler(error);
-  });
-});
-$('#addNewSoftware').click(() => {
-  let _csrf = $('#csrfToken').val();
-  let name = $('#softwares').val();
-  $.post('/api/candidate/softwares/add', {name, _csrf}, (data) => {
-    if (data.software) {
-      $('#softwares').val('');
-      notification({
-        icon: 'check-circle',
-        type: 'success',
-        title: 'Logiciel ajouté avec succès :',
-        message: `Le logiciel "${data.software.name}" vient d'être ajouté à vos compétences.`
-      });
-      $("#softwaresList").append($('<tr>')
-        .attr({'data-type': 'software', 'data-id': data.software.id})
-        .append($('<td>').text(`${data.software.name}`))
-        .append($('<td>').append($('<div>').attr({class: 'rating-stars text-center'})
-          .append($('<ul>').attr({
-            class: 'stars',
-            'data-type': 'software',
-            'data-id': data.software.id
-          })
-            .append($('<li>').attr({class: 'star', title: 'Fair', 'data-value': 1}).html('<i class="fa fa-star"></i>'))
-            .append($('<li>').attr({class: 'star', title: 'Good', 'data-value': 2}).html('<i class="fa fa-star"></i>'))
-            .append($('<li>').attr({class: 'star', title: 'Excellent', 'data-value': 3}).html('<i class="fa fa-star"></i>')))
-        ))
-        .append($('<td>').append($('<button>').attr({
-          class: 'btn btn-simple btn-danger btn-icon remove',
-          'data-type': 'software',
-          'data-id': data.software.id
-        }).html('<i class="fa fa-trash"></i>')))
-      );
-    }
-  }).catch(error => {
-    error = error.responseJSON;
-    if (error.error === 'Already exists') {
-      notification({
-        icon: 'exclamation',
-        type: 'danger',
-        title: 'Ajout impossible',
-        message: `Ce logiciel est déjà ajouté à votre profil.`
-      });
-    } else return errorsHandler(error);
+  let messageSuccess, messageError, titleSuccess;
+  $(`#addNew${eCapitalize}`).click(() => {
+    let name = $(`#${ac}`).val();
+    let _csrf = $('#csrfToken').val();
+    $.post(`/api/candidate/${ac}/add`, { name, _csrf }, (data) => {
+      switch (ac) {
+        case 'skills':
+          messageSuccess = `Cette compétence "${data[e].name}" vient d'être ajoutée à votre liste.`;
+          titleSuccess = 'Compétence ajoutée avec succès :';
+          break;
+        case 'equipments':
+          messageSuccess = `Ce matériel ou produit "${data[e].name}" vient d'être ajouté à votre liste.`;
+          titleSuccess = 'Matériel / produit ajouté avec succès :';
+          break;
+        case 'softwares':
+          messageSuccess = `Ce logiciel "${data[e].name}" vient d'être ajouté à votre liste.`;
+          titleSuccess = 'Logiciel ajouté avec succès :';
+          break;
+      }
+      if (data[e]) {
+        $(`#${ac}`).val('');
+        notification({
+          icon: 'check-circle',
+          type: 'success',
+          title: titleSuccess,
+          message: messageSuccess
+        });
+        $(`#${ac}List`).append($('<tr>')
+          .attr({'data-type': e, 'data-id': data[e].id})
+          .append($('<td>').text(`${data[e].name}`))
+          .append($('<td>').append($('<div>').attr({class: 'rating-stars text-center'})
+            .append($('<ul>').attr({
+              class: 'stars',
+              'data-type': e,
+              'data-id': data[e].id
+            })
+              .append($('<li>').attr({class: 'star', title: 'Fair', 'data-value': 1}).html('<i class="fa fa-star"></i>'))
+              .append($('<li>').attr({class: 'star', title: 'Good', 'data-value': 2}).html('<i class="fa fa-star"></i>'))
+              .append($('<li>').attr({class: 'star', title: 'Excellent', 'data-value': 3}).html('<i class="fa fa-star"></i>')))
+          ))
+          .append($('<td>').append($('<button>').attr({
+            class: 'btn btn-simple btn-danger btn-icon remove',
+            'data-type': e,
+            'data-id': data[e].id
+          }).html('<i class="fa fa-trash"></i>')))
+        );
+      }
+    }).catch(error => {
+      error = error.responseJSON;
+      switch (ac) {
+        case 'skills':
+          messageError = `Cette compétence est déjà ajoutée à votre profil.`;
+          break;
+        case 'equipments':
+          messageError = `Ce matériel ou produit est déjà ajouté à votre profil.`;
+          break;
+        case 'softwares':
+          messageError = `Ce logiciel est déjà ajouté à votre profil.`;
+          break;
+      }
+      if (error.error === 'Already exists') {
+        notification({
+          icon: 'exclamation',
+          type: 'danger',
+          title: 'Ajout impossible',
+          message: messageError
+        });
+      } else return errorsHandler(error);
+    });
   });
 });
 
@@ -214,7 +101,18 @@ $('body').on('click', '.remove', (event) => {
   $("#removeSkillModal").modal();
 });
 
+let autocompleteTrigger = (elements, contain) => {
+  if ($(`tr[data-type="${elements.type}"]>td:contains('${contain}')`).length > 0) {
+    $(`#${elements.add}`).attr('disabled', 'disabled');
+    $(`#${elements.already}`).show();
+  } else {
+    $(`#${elements.add}`).removeAttr('disabled');
+    $(`#${elements.already}`).hide();
+  }
+};
+
 let removeSkill = (type, id) => {
+  let _csrf = $('#csrfToken').val();
   $.delete(`/api/candidate/${type}/${id}`, {_csrf}, (data) => {
     if (data.deleted) {
       notification({
