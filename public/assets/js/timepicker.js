a154493c6ed8acb8d3b7d4e3eb2887272c20aca7
Сорок два <garcia.llyam@gmail.com>
2019-01-11 16:41:58 +0100

    refactoring + add from / to listeners

--- public/assets/js/timepicker.js
+++ public/assets/js/timepicker.js
@@ -1,66 +1,49 @@
 $(document).ready(() => {
+  let icons = {
+    time: "fa fa-clock-o",
+    date: "fa fa-calendar",
+    up: "fa fa-chevron-up",
+    down: "fa fa-chevron-down",
+    previous: 'fa fa-chevron-left',
+    next: 'fa fa-chevron-right',
+    today: 'fa fa-screenshot',
+    clear: 'fa fa-trash',
+    close: 'fa fa-remove'
+  };
 
-  $('.datetimepicker').datetimepicker({
-    icons: {
-      time: "fa fa-clock-o",
-      date: "fa fa-calendar",
-      up: "fa fa-chevron-up",
-      down: "fa fa-chevron-down",
-      previous: 'fa fa-chevron-left',
-      next: 'fa fa-chevron-right',
-      today: 'fa fa-screenshot',
-      clear: 'fa fa-trash',
-      close: 'fa fa-remove'
-    }
-  });
+  let datePickerOpts = {
+    format: 'DD/MM/YYYY',
+    useCurrent: false,
+    icons
+  };
 
-  $('.datepicker').datetimepicker({
-    format: 'MM/DD/YYYY',
-    icons: {
-      time: "fa fa-clock-o",
-      date: "fa fa-calendar",
-      up: "fa fa-chevron-up",
-      down: "fa fa-chevron-down",
-      previous: 'fa fa-chevron-left',
-      next: 'fa fa-chevron-right',
-      today: 'fa fa-screenshot',
-      clear: 'fa fa-trash',
-      close: 'fa fa-remove'
-    }
-  });
+  $('.datetimepicker').datetimepicker({ icons });
+
+  $('.datepicker').datetimepicker(datePickerOpts);
 
   $('.monthpicker').datetimepicker({
     format: 'MM/YYYY',
     useCurrent: false,
-    icons: {
-      time: "fa fa-clock-o",
-      date: "fa fa-calendar",
-      up: "fa fa-chevron-up",
-      down: "fa fa-chevron-down",
-      previous: 'fa fa-chevron-left',
-      next: 'fa fa-chevron-right',
-      today: 'fa fa-screenshot',
-      clear: 'fa fa-trash',
-      close: 'fa fa-remove'
-    }
+    icons
   });
 
   $('.timepicker').datetimepicker({
 //          format: 'H:mm',    // use this format if you want the 24hours timepicker
     format: 'h:mm A',    //use this format if you want the 12hours timpiecker with AM/PM toggle
-    icons: {
-      time: "fa fa-clock-o",
-      date: "fa fa-calendar",
-      up: "fa fa-chevron-up",
-      down: "fa fa-chevron-down",
-      previous: 'fa fa-chevron-left',
-      next: 'fa fa-chevron-right',
-      today: 'fa fa-screenshot',
-      clear: 'fa fa-trash',
-      close: 'fa fa-remove'
-    }
+    icons
   });
 
+  //from to Datepicker rules
+  $('.from').datetimepicker(datePickerOpts).on('dp.change', (e) => {
+    let incrementDay = moment(new Date(e.date));
+    incrementDay.add(1, 'days');
+    $('.to').data('DateTimePicker').minDate(incrementDay);
+  });
+  $('.to').datetimepicker(datePickerOpts).on('dp.change',  (e) => {
+    let decrementDay = moment(new Date(e.date));
+    decrementDay.subtract(1, 'days');
+    $('.from').data('DateTimePicker').maxDate(decrementDay);
+  });
   //XP Datepicker
   $('#xpFrom').datetimepicker().on('dp.change', (e) => {
     let incrementDay = moment(new Date(e.date));
