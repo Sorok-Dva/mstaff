e1676b632af55bfb38acbf1191351e7cad00acb5
Сорок два <garcia.llyam@gmail.com>
2019-01-12 03:15:24 +0100

    add rolify handlebars helper + rename helpers/index file to handlebars

---
+++ helpers/handlebars.js
@@ -0,0 +1,88 @@
+/**
+ * Handlebars helpers
+ */
+const moment = require('moment');
+
+module.exports.register = async (Handlebars) => {
+  Handlebars.registerHelper('date', (context, format) => {
+    return moment(new Date(context)).format(format);
+  });
+
+  Handlebars.registerHelper('age', (context, format) => {
+    return moment().diff(new Date(context), format);
+  });
+
+  Handlebars.registerHelper('number', (number) => {
+    if (number) {
+      return number.toLocaleString();
+    } else {
+      return number;
+    }
+  });
+
+  Handlebars.registerHelper('rolify', (role) => {
+    switch (role) {
+    case 'User': return 'Utilisateur';
+    case 'Admin': return 'Administrateur';
+    case 'admin': return 'Administrateur';
+    case 'es': return 'Établissement';
+    case 'demo': return 'Démo';
+    case 'candidate': return 'Candidat';
+    default: return role
+    }
+  });
+
+  Handlebars.registerHelper('rating', (stars, star) => {
+    if (star <= stars) {
+      return 'selected';
+    }
+  });
+
+  Handlebars.registerHelper('ifCond', (v1, operator, v2, options) => {
+    switch (operator) {
+    case '==':
+      return (v1 == v2) ? options.fn(this) : options.inverse(this);
+    case '===':
+      return (v1 === v2) ? options.fn(this) : options.inverse(this);
+    case '!=':
+      return (v1 != v2) ? options.fn(this) : options.inverse(this);
+    case '!==':
+      return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+    case '<':
+      return (v1 < v2) ? options.fn(this) : options.inverse(this);
+    case '<=':
+      return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+    case '>':
+      return (v1 > v2) ? options.fn(this) : options.inverse(this);
+    case '>=':
+      return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+    case '&&':
+      return (v1 && v2) ? options.fn(this) : options.inverse(this);
+    case '||':
+      return (v1 || v2) ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper('math', (lvalue, operator, rvalue) => {
+    lvalue = parseFloat(lvalue);
+    rvalue = parseFloat(rvalue);
+
+    return {
+      '+': lvalue + rvalue,
+      '-': lvalue - rvalue,
+      '*': lvalue * rvalue,
+      '/': lvalue / rvalue,
+      '%': lvalue % rvalue
+    }[operator];
+  });
+
+  Handlebars.registerHelper('has_passed', (dateString, options) => {
+    if(moment(dateString).isAfter(moment())) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+};
