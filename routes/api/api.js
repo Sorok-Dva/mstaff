ba26a3aff6698eba161110cdae8ec27728a174bc
Сорок два <garcia.llyam@gmail.com>
2019-01-16 13:21:51 +0100

    add findByGeo method in api route

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const ApiController = require('../../controllers/api');
+const ESController = require('../../controllers/establishment');
 
 router.get('/', (req, res) => res.json({ message: 'welcome' }));
 router.get('/skills/all', ApiController.getSkillsList);
 router.get('/equipments/all', ApiController.getEquipmentsList);
 router.get('/softwares/all', ApiController.getSoftwaresList);
+router.post('/establishments/findByGeo', ESController.findByGeo);
 
 module.exports = router;
