83f45a661563153816974b0ec60cbaa0975a0cdb
Сорок два <garcia.llyam@gmail.com>
2019-01-18 15:39:38 +0100

    finish upload candidate video front script

--- public/javascripts/uploadVideo.js
+++ public/javascripts/uploadVideo.js
@@ -1,124 +1,137 @@
-let videoUpload = () => {
-  let Init = () => {
-    let fileSelect = document.getElementById('file-upload'),
-      fileDrag = document.getElementById('file-drag');
+let file = null;
+let fileDragHover = (e) => {
+  let fileDrag = document.getElementById('file-drag');
 
-    fileSelect.addEventListener('change', fileSelectHandler, false);
+  e.stopPropagation();
+  e.preventDefault();
+  fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
+};
 
-    let xhr = new XMLHttpRequest();
-    if (xhr.upload) {
-      fileDrag.addEventListener('dragover', fileDragHover, false);
-      fileDrag.addEventListener('dragleave', fileDragHover, false);
-      fileDrag.addEventListener('drop', fileSelectHandler, false);
-    }
-  };
+let fileSelectHandler = (e) => {
+  // Fetch FileList object
+  let files = e.target.files || e.dataTransfer.files;
 
-  let fileDragHover = (e) => {
-    let fileDrag = document.getElementById('file-drag');
+  // Cancel event and hover styling
+  fileDragHover(e);
 
-    e.stopPropagation();
-    e.preventDefault();
-    fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
-  };
+  if (files.length > 1) {
+    output(`<strong>Vous ne pouvez uploader qu'une vidéo.</strong>`);
+  } else {
+    parseFile(files[0]);
+  }
+};
 
-  let fileSelectHandler = (e) => {
-    // Fetch FileList object
-    let files = e.target.files || e.dataTransfer.files;
+let output = (msg) => $('#messages').html(msg);
 
-    // Cancel event and hover styling
-    fileDragHover(e);
+let parseFile = (f) => {
+  file = f;
+  output('<strong>' + encodeURI(file.name) + '</strong>');
 
-    if (files.length > 1) {
-      output(`<strong>Vous ne pouvez uploader qu'une vidéo.</strong>`);
+  let imageName = file.name;
+  let $source = $('#videoSrc');
+
+  let isGood = (/\.(?=mp4)/gi).test(imageName);
+  if (isGood) {
+    $('#start').hide();
+    $('#notvideo').hide();
+    $('#response').show();
+    $('#video-preview').show();
+    $source[0].src = URL.createObjectURL(file);
+    $source.parent()[0].load();
+    $source.attr('data-type', 'preview');
+
+    $('#file-progress').attr('max', file.size);
+  }
+  else {
+    output(`<strong>Mauvais format. Formats autorisé : mp4</strong>`);
+    $('#start').show();
+    $('#notvideo').show();
+    $('#response').hide();
+    $('#video-preview').hide();
+    document.getElementById("file-upload-form").reset();
+  }
+};
+
+let updateFileProgress = (e) => {
+  if (e.lengthComputable)  {
+    (e.lengthComputable) ? $('#file-progress').attr('value', e.loaded) : null;
+  }
+};
+
+function uploadFile(f) {
+  file = f;
+  let xhr = new XMLHttpRequest(),
+    fileInput = document.getElementById('class-roster-file'),
+    pBar = document.getElementById('file-progress'),
+    fileSizeLimit = 10240; // In MB
+  if (xhr.upload) {
+    // Check if file is less than x MB
+    if (file.size <= fileSizeLimit * 10240 * 1024) {
+      // Progress bar
+      pBar.style.display = 'inline';
+      xhr.upload.addEventListener('progress', updateFileProgress, false);
+
+      // File received / failed
+      xhr.onreadystatechange = function(e) {
+        $('#file-progress').attr('class', `progress ${(xhr.status === 200 ? "success" : "failure")}`);
+
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          notification({
+            icon: 'check-circle',
+            type: 'success',
+            title: 'Vidéo de profil sauvegardée :',
+            message: `Votre vidéo de présentation a correctement été ajoutée à votre profil.`
+          });
+        }
+      };
+
+      // Start upload
+      xhr.open('POST', document.getElementById('file-upload-form').action, true);
+      xhr.setRequestHeader('x-csrf-token', $('#csrfToken').val());
+      xhr.setRequestHeader('X-File-Name', file.name);
+      xhr.setRequestHeader('X-File-Size', file.size);
+      let formdata = new FormData();
+      formdata.append('file', file);
+      xhr.send(formdata);
     } else {
-      parseFile(files[0]);
+      output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).');
     }
-  };
+  }
+}
 
-  let output = (msg) => $('#messages').html(msg);
+if (window.File && window.FileList && window.FileReader) {
+  let fileSelect = document.getElementById('file-upload'),
+    videoPreview = document.getElementById('video-preview'),
+    fileDrag = document.getElementById('file-drag');
 
-  let parseFile = (file) => {
-    output('<strong>' + encodeURI(file.name) + '</strong>');
+  fileSelect.addEventListener('change', fileSelectHandler, false);
 
-    let imageName = file.name;
-    let $source = $('#videoSrc');
-
-    let isGood = (/\.(?=mp4|avi|wmv|mov|wmf)/gi).test(imageName);
-    if (isGood) {
-      $('#start').hide();
-      $('#notvideo').hide();
-      $('#response').show();
-      $('#video-preview').show();
-      // Video Preview
-      $source[0].src = URL.createObjectURL(file);
-      $source.parent()[0].load();
-      $('#file-progress').attr('max', file.size);
+  videoPreview.addEventListener('loadedmetadata', () => {
+    if ($('#videoSrc').attr('data-type') !== 'preview') return false;
+    let duration = $('#videoSrc').parent()[0].duration.toFixed(2);
+    if (duration <= 600) {
       uploadFile(file);
-    }
-    else {
+    } else {
+      output(`<strong>Vidéo trop longue. Durée maximale autorisée : 10 minutes.</strong>`);
       $('#start').show();
-      $('#notvideo').show();
       $('#response').hide();
       $('#video-preview').hide();
       document.getElementById("file-upload-form").reset();
+      notification({
+        icon: 'exclamation',
+        type: 'danger',
+        title: 'Impossible d\'enregistrer cette vidéo :',
+        message: `La durée de votre vidéo est plus grand que la limite autorisée : 10 minutes.`
+      });
     }
-  };
+  });
 
-  let updateFileProgress = (e) => {
-    if (e.lengthComputable)  {
-      console.log(`load set to ${e.loaded}`);
-      (e.lengthComputable) ? $('#file-progress').attr('value', e.loaded) : null;
-
-    }
-  };
-
-  function uploadFile(file) {
-
-    let xhr = new XMLHttpRequest(),
-      fileInput = document.getElementById('class-roster-file'),
-      pBar = document.getElementById('file-progress'),
-      fileSizeLimit = 10240; // In MB
-    console.log(xhr);
-    if (xhr.upload) {
-      console.log('xhr.upload');
-      // Check if file is less than x MB
-      if (file.size <= fileSizeLimit * 10240 * 1024) {
-        // Progress bar
-        pBar.style.display = 'inline';
-        xhr.upload.addEventListener('progress', updateFileProgress, false);
-
-        // File received / failed
-        xhr.onreadystatechange = function(e) {
-          $('#file-progress').attr('class', `progress ${(xhr.status === 200 ? "success" : "failure")}`);
-
-          if (xhr.readyState === 4) {
-            notification({
-              icon: 'check-circle',
-              type: 'success',
-              title: 'Vidéo de profil sauvegardée :',
-              message: `Votre vidéo de présentation a correctement été ajoutée à votre profil.`
-            });
-          }
-        };
-
-        // Start upload
-        xhr.open('POST', document.getElementById('file-upload-form').action, true);
-        xhr.setRequestHeader('x-csrf-token', $('#csrfToken').val());
-        xhr.setRequestHeader('X-File-Name', file.name);
-        xhr.setRequestHeader('X-File-Size', file.size);
-        let formdata = new FormData();
-        formdata.append('file', file);
-        xhr.send(formdata);
-      } else {
-        output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).');
-      }
-    }
+  let xhr = new XMLHttpRequest();
+  if (xhr.upload) {
+    fileDrag.addEventListener('dragover', fileDragHover, false);
+    fileDrag.addEventListener('dragleave', fileDragHover, false);
+    fileDrag.addEventListener('drop', fileSelectHandler, false);
   }
-
-  if (window.File && window.FileList && window.FileReader) {
-    Init();
-  } else {
-    document.getElementById('file-drag').style.display = 'none';
-  }
-};
-videoUpload();
+} else {
+  document.getElementById('file-drag').style.display = 'none';
+}
