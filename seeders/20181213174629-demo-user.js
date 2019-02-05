911e9c98f7d106ae627e60b4f3124fa0c0ed6549
Сорок два <garcia.llyam@gmail.com>
2019-02-05 16:08:21 +0100

    improve user seeder with random first name and last name

--- seeders/20181213174629-demo-user.js
+++ seeders/20181213174629-demo-user.js
@@ -1,19 +1,29 @@
 'use strict';
 
+const _ = require('lodash');
+
+let rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+let firstNames = ['John', 'Lisa', 'Lucas', 'Maelis', 'Laura', 'Baptiste', 'William', 'Benjamin', 'Pauline', 'Charline', 'Anita', 'Marie'];
+let lastNames = ['Doe', 'Dupont', 'Laporte', 'Boulanger', 'Saurin', 'Lepetit', 'Legrand', 'Curie', 'Jaipadydai', 'Ifaibo'];
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'demo@demo.com',
-      password: 'passwordBidon',
-      birthday: new Date(1998, 7, 12),
-      postal_code: '75000',
-      town: 'PARIS',
-      phone: '+33700004242',
-      role: 'User',
-      type: 'candidate'
-    }], {});
+    let records = [];
+    for (let i = 0; i < 25; i++) {
+      records.push({
+        firstName: _.sample(firstNames),
+        lastName: _.sample(lastNames),
+        email: `seeder-${i}@demo.com`,
+        password: 'passwordBidon',
+        birthday: new Date(rand(1950, 2001), rand(1, 12), rand(1, 30)),
+        postal_code: '75000',
+        town: 'PARIS',
+        phone: `+33700${Math.floor(Math.random() * 999999) + 100000  }`,
+        role: 'User',
+        type: 'candidate'
+      });
+    }
+    return queryInterface.bulkInsert('Users', records, {});
   },
 
   down: (queryInterface, Sequelize) => {
