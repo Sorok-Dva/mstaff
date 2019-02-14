d0869863df848d65fa56f3e01ab306bbfae96e02
Сорок два <garcia.llyam@gmail.com>
2019-02-14 15:21:44 +0100

    change datetimepicker config + improve timepicker

--- public/assets/js/timepicker.js
+++ public/assets/js/timepicker.js
@@ -1,36 +1,21 @@
 $(document).ready(() => {
-  let icons = {
-    time: "fa fa-clock-o",
-    date: "fa fa-calendar",
-    up: "fa fa-chevron-up",
-    down: "fa fa-chevron-down",
-    previous: 'fa fa-chevron-left',
-    next: 'fa fa-chevron-right',
-    today: 'fa fa-screenshot',
-    clear: 'fa fa-trash',
-    close: 'fa fa-remove'
-  };
-
   let datePickerOpts = {
     format: 'DD/MM/YYYY',
-    useCurrent: false,
-    icons
+    useCurrent: false
   };
 
-  $('.datetimepicker').datetimepicker({ icons });
+  $('.datetimepicker').datetimepicker();
 
   $('.datepicker').datetimepicker(datePickerOpts);
 
   $('.monthpicker').datetimepicker({
     format: 'MM/YYYY',
-    useCurrent: false,
-    icons
+    useCurrent: false
   });
 
   $('.timepicker').datetimepicker({
 //          format: 'H:mm',    // use this format if you want the 24hours timepicker
     format: 'h:mm A',    //use this format if you want the 12hours timpiecker with AM/PM toggle
-    icons
   });
 
   //from to Datepicker rules
