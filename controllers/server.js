c5517f18b29da87348eb06a4aa163267d351a46c
Сорок два <garcia.llyam@gmail.com>
2019-02-20 18:13:43 +0100

    update verify result to live

--- controllers/server.js
+++ controllers/server.js
@@ -3,6 +3,6 @@ const Models = require('../models/index');
 module.exports = {
   verifyMaintenance: callback => {
     // @todo create server table and take value from it
-    return callback('maintenance');
+    return callback('live');
   }
 };
