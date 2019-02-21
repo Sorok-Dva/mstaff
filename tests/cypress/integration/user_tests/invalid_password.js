49ff5cfaaedf819904beb7edb591ab006f5f47bd
404 <luc@mstaff.co>
2019-02-21 13:10:19 +0100

    cypress integration, some basic tests, and favicon to remove 'file not found' error

---
+++ tests/cypress/integration/user_tests/invalid_password.js
@@ -0,0 +1,26 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+});
+
+describe('register as new user', function () {
+  it('register', function () {
+    cy.visit('localhost:3000/register')
+      .get('#firstName')
+      .type('Johnny')
+      .get('#lastName')
+      .type('Doe')
+      .get('#birthday')
+      .type('1998-10-10')
+      .get('#postal_code')
+      .type('75017')
+      .get('#town')
+      .type('Paris')
+      .get('#phone')
+      .type('0612345678')
+      .get('#email')
+      .type('john@doe.net')
+      .get('#password')
+      .type('123')
+      .get('form').find('button').contains('inscrire').click()
+  })
+})
