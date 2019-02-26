baf3b62f9a723ef6111fef5b8adf2822c2c611e3
Сорок два <garcia.llyam@gmail.com>
2019-02-26 16:47:22 +0100

    add utilisateur -> Users tables migration

--- tools/db-migration/app.js
+++ tools/db-migration/app.js
@@ -30,4 +30,45 @@ pgsql.connect(err => {
   }
 });
 
+let migrate = {};
+
+migrate.users = () => {
+  pgsql.get({
+    name: 'get-users',
+    text: 'SELECT * FROM utilisateur'
+  }, (err, users) => {
+    if (err) console.log(err);
+    mysql.get('mstaff', (err, con) => {
+      if (err) console.log(err);
+      for (let i = 0; i < users.rows.length; i++) {
+        let user = users.rows[i];
+        // get candidate associated to user
+        pgsql.get({
+          name: 'get-candidate', text: 'SELECT * FROM candidat WHERE utilisateur_id = $1', values: [user.id]
+        }, (err, candidat) => {
+          console.log(user);
+          user.candidate = candidat.rows[0];
+          let data = {
+            email: user.email,
+            password: user.password,
+            type: userType(user.type),
+            firstName: user.prenom || user.candidate ? user.candidate.prenom : null,
+            lastName: user.nom || user.candidate ? user.candidate.nom : null,
+            birthday: user.candidate ? user.candidate.date_naissance : null,
+            createdAt: user.created_at,
+            updatedAt: user.updated_at
+          };
+          con.query('INSERT INTO Users SET ?', data);
+        });
+      }
+    });
+  });
+};
+
+let userType = (type) => {
+  if (type === 'CANDIDAT') return 'candidate';
+  if (type === 'ETABLISSEMENT') return 'es';
+  if (type === 'PERSONNEL_INTERNE') return '';
+};
+
 module.exports = app;
