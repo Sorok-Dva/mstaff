b870fdec9651da53c5220873519fc2f64f7906c1
Сорок два <garcia.llyam@gmail.com>
2019-02-27 13:51:28 +0100

    add logger

--- tools/db-migration/app.js
+++ tools/db-migration/app.js
@@ -33,6 +33,7 @@ pgsql.connect(err => {
 let migrate = {};
 
 migrate.users = () => {
+  log('GET PgSQL Users Data ("utilisateur" table)');
   pgsql.get({
     name: 'get-users',
     text: 'SELECT * FROM utilisateur'
@@ -40,32 +41,11 @@ migrate.users = () => {
     if (err) console.log(err);
     mysql.get('mstaff', (err, con) => {
       if (err) console.log(err);
+      log(`${users.rows.length} rows founded.`);
       for (let i = 0; i < 25; i++) {
         let user = users.rows[i];
         // get candidate associated to user
-        pgsql.get({
-          name: 'get-candidate', text: 'SELECT * FROM candidat WHERE utilisateur_id = $1', values: [user.id]
-        }, (err, candidat) => {
-          let candidate = candidat.rows[0];
-          let UserData = {
-            email: user.email,
-            password: user.password,
-            type: userType(user.type),
-            firstName: user.prenom,
-            lastName: user.nom,
-            createdAt: user.created_at || new Date(),
-            updatedAt: user.updated_at || new Date(),
-          };
-          if (candidat.rows.length === 1) {
-            UserData.firstName = candidate.prenom;
-            UserData.lastName = candidate.nom;
-            UserData.birthday = candidate.date_naissance;
-            UserData.postal_code = candidate.code_postal;
-            UserData.town = candidate.ville;
-            UserData.phone = candidate.telephone;
-            UserData.createdAt = candidate.created_at || new Date();
-            UserData.updatedAt = candidate.updated_at || new Date();
-          }
+        this.candidates(user, (UserData) => {
           con.query('INSERT INTO Users SET ?', UserData, (err, userRes) => {
             if (err) {
               if (err.code === 'ER_DUP_ENTRY') console.log('[DUPLICATION] ', err.sqlMessage)
@@ -87,9 +67,38 @@ migrate.users = () => {
               }
             }
           });
-        });
+        })
       }
     });
+  });
+};
+
+migrate.candidates = (user, callback) => {
+  log(`GET PgSQL Candidate Data ("candidat" table) of user id ${user.id}`);
+  pgsql.get({
+    name: 'get-candidate', text: 'SELECT * FROM candidat WHERE utilisateur_id = $1', values: [user.id]
+  }, (err, candidat) => {
+    let candidate = candidat.rows[0];
+    let UserData = {
+      email: user.email,
+      password: user.password,
+      type: userType(user.type),
+      firstName: user.prenom,
+      lastName: user.nom,
+      createdAt: user.created_at || new Date(),
+      updatedAt: user.updated_at || new Date(),
+    };
+    if (candidat.rows.length === 1) {
+      UserData.firstName = candidate.prenom;
+      UserData.lastName = candidate.nom;
+      UserData.birthday = candidate.date_naissance;
+      UserData.postal_code = candidate.code_postal;
+      UserData.town = candidate.ville;
+      UserData.phone = candidate.telephone;
+      UserData.createdAt = candidate.created_at || new Date();
+      UserData.updatedAt = candidate.updated_at || new Date();
+    }
+    return callback(UserData);
   });
 };
 
@@ -100,6 +109,11 @@ let userType = (type) => {
   console.log(type); return '';
 };
 
+let log = (msg) => {
+  console.log('[DB-MIGRATION]', msg);
+};
+
+log('Starting DBs Migration');
 migrate.users();
 
 module.exports = app;
