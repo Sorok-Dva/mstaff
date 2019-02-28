dd73d62ff486cd4d6963b533379d08cb15327725
Сорок два <garcia.llyam@gmail.com>
2019-02-28 17:42:51 +0100

    revert d2b5430

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('../bin/passport');
 const middleware = require('../middlewares');
 
 const UserController = require('../controllers/user');
