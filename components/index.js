efdb63ed5d45c830609864a481354d2f0266f739
Сорок два <garcia.llyam@gmail.com>
2019-03-19 15:23:10 +0100

    add components/index

---
+++ components/index.js
@@ -0,0 +1,13 @@
+const BackOffice = require('./back-office');
+const Establishment = require('./establishment');
+const Mailer = require('./mailer');
+const Render = require('./render');
+const User = require('./user');
+
+module.exports = {
+  BackOffice,
+  Establishment,
+  Mailer,
+  Render,
+  User
+};
