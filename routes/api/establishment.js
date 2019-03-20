4ef8f6b689241649e273fd76e5f5cb2b99534520
Сорок два <garcia.llyam@gmail.com>
2019-03-20 17:24:47 +0100

    add digital regex on id parameters in es api routes

--- routes/api/establishment.js
+++ routes/api/establishment.js
@@ -8,7 +8,7 @@ router.post('/:esId(\\d+)/search/candidates',
   Controller.Establishment.Application.getCandidates
 );
 
-router.post('/:esId(\\d+)/get/user/:userId',
+router.post('/:esId(\\d+)/get/user/:userId(\\d+)',
   Authentication.ensureIsEs && Authentication.verifyEsAccess,
   Controller.apiGetCandidate
 );
@@ -17,12 +17,12 @@ router.post('/:esId(\\d+)/addNeed',
   Authentication.ensureIsEs && Authentication.verifyEsAccess,
   Controller.Establishment.Need.Create);
 
-router.post('/:esId(\\d+)/need/:id(\\d+)/:action/user/:candidateId',
+router.post('/:esId(\\d+)/need/:id(\\d+)/:action/user/:candidateId(\\d+)',
   Authentication.ensureIsEs && Authentication.verifyEsAccess,
   Controller.apiNeedCandidate
 );
 
-router.post('/:esId(\\d+)/user/:candidateId/:action',
+router.post('/:esId(\\d+)/candidate/:candidateId(\\d+)/:action',
   Authentication.ensureIsEs && Authentication.verifyEsAccess,
   Controller.apiFavCandidate
 );
