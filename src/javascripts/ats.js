92c519023dc1ad5274e932e6d738de3cc5d85820
Kronox21 <piccolo.rom@gmail.com>
2019-03-20 20:08:34 +0100

    selectPosts ats modal

---
+++ src/javascripts/ats.js
@@ -0,0 +1,11 @@
+let createPostModal = () => {
+  console.log('coucou');
+  $('#postModal').show();
+};
+
+let dismissModal = () => {
+  $('#postModal').hide()
+};
+
+$(document).ready(function () {
+});
