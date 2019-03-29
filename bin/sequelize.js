1c58b01ed8a24f450454b2a0e45f913c3918c444
Сорок два <garcia.llyam@gmail.com>
2019-03-29 14:51:29 +0100

    fix sequelize config call

--- bin/sequelize.js
+++ bin/sequelize.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
-const conf = require('dotenv').config().parsed;
-const env = conf.ENV;
-const config = require(`../orm/config/config.json`)[env];
+const { Env } = require('../helpers/helpers');
+const config = require(`${__dirname}/../orm/config/config.json`)[Env.current];
 
 module.exports = new Sequelize(
   config.database, config.username, config.password, config
