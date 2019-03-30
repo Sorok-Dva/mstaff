510b0fafbcdaf0281555f80476ce151d6f2ad1d6
Сорок два <garcia.llyam@gmail.com>
2019-03-30 16:55:45 +0100

    try to fix es migration

--- tools/db-migration/app.js
+++ tools/db-migration/app.js
@@ -76,8 +76,8 @@ mysql.get('mstaff', (err, con) => {
               if (err) {
                 if (err.code === 'ER_DUP_ENTRY') console.log('[DUPLICATION] ', err.sqlMessage)
               } else {
-                if (establishments.indexOf(user.es_id) === -1) {
-                  migrate.searchAndMigrateES(user.es_id);
+                if (_.isNil(establishments.find((data) => data.oldId === user.es_id).id)) {
+                  migrate.searchAndMigrateES(user.es_id, userRes.insertId);
                 } else {
                   migrate.createESAccount(user.es_id, userRes.insertId);
                 }
@@ -111,7 +111,7 @@ mysql.get('mstaff', (err, con) => {
     });
   };
 
-  migrate.searchAndMigrateES = (es_id, callback) => {
+  migrate.searchAndMigrateES = (es_id, userRes) => {
     establishments.push(es_id);
     log(`GET PgSQL Establishment Data ("etablissement" table) of es id ${es_id}`);
     pgsql.get({
@@ -143,6 +143,7 @@ mysql.get('mstaff', (err, con) => {
           console.log(err);
         } else {
           establishments[es_id] = { id: esRes.insertId, oldId: es_id };
+          migrate.createESAccount(user.es_id, userRes.insertId);
         }
       });
     });
