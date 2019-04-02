ecccf6cb99c041479e524056883ced0ad5703a60
Сорок два <garcia.llyam@gmail.com>
2019-04-02 15:31:57 +0200

    add needFeedback mysql schema migration and model

---
+++ orm/migrations/20190402131616-create-need-feedback.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('NeedFeedbacks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      es_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Establishments',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      how: {
+        type: Sequelize.STRING
+      },
+      candidates: {
+        type: Sequelize.JSON
+      },
+      stars: {
+        type: Sequelize.INTEGER
+      },
+      feedback: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('NeedFeedbacks');
+  }
+};
