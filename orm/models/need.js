915bdea277a6f2e7b72a37deaf2dbc1285fc3062
Сорок два <garcia.llyam@gmail.com>
2019-04-02 15:58:04 +0200

    add closed column in Need migration and model

--- orm/models/need.js
+++ orm/models/need.js
@@ -6,7 +6,8 @@ module.exports = (sequelize, DataTypes) => {
     post: DataTypes.STRING,
     contract_type: DataTypes.STRING,
     start: DataTypes.DATE,
-    end: DataTypes.DATE
+    end: DataTypes.DATE,
+    closed: DataTypes.BOOLEAN,
   }, {});
   Need.associate = function (models) {
     Need.hasOne(models.Establishment, {
