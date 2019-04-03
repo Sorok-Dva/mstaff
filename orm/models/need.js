3211e9bc5a23cc928fb68437fe266235ce39bc7f
Сорок два <garcia.llyam@gmail.com>
2019-04-03 17:25:09 +0200

    update Need orm model (add createdBy column)

--- orm/models/need.js
+++ orm/models/need.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Need = sequelize.define('Need', {
     name: DataTypes.STRING,
     es_id: DataTypes.INTEGER,
+    createdBy: DataTypes.INTEGER,
     post: DataTypes.STRING,
     contract_type: DataTypes.STRING,
     start: DataTypes.DATE,
