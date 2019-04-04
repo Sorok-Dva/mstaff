2a32d0e3e08d960758939d75a520484d29e4707d
Сорок два <garcia.llyam@gmail.com>
2019-04-04 18:10:28 +0200

    add appNotifications front javascript

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -149,4 +149,179 @@ $(document).ready(function () {
   $('.stop-step').click(function (e) {
     history.back()
   });
+
+  appNotifications.init();
+  $('#notificationsContainer').on('click', '.notif-li', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    let notifId = $(event.currentTarget).attr('data-notif-id');
+    $('#notificationsContainer').hide();
+
+    $.get(`/api/user/notification/${notifId}`, (data) => {
+      loadTemplate('/static/views/notification/main.hbs', data, (html) => {
+        $('#notificationData').html(html).show();
+        $('#notif-see-all').hide();
+        $('#notif-back').show();
+      });
+    });
+  });
+
+  $('#notif-back').click(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('#notificationsContainer').show();
+    $('#notificationData').hide();
+    $('#notif-see-all').show();
+    $('#notif-back').hide();
+  });
 });
+
+let count = 0;
+let lastCount = 0;
+let appNotifications = {
+  init: () => {
+    $("#notificationsBadge").hide();
+    $("#notificationAucune").hide();
+
+    // bind click on notifications
+    $("#notifications-dropdown").on('click', function () {
+      let open = $("#notifications-dropdown").attr("aria-expanded");
+
+      if (open === "false") {
+        appNotifications.loadAll();
+      }
+    });
+
+    // Load notifications
+    appNotifications.loadAll();
+
+    // Polling
+    // Every 3 minutes we check if new notifications
+    setInterval(function () {
+      appNotifications.loadNumber();
+    }, 180000);
+
+    // Binding de marquage comme lue desktop
+    $('.notification-read-desktop').on('click', function (event) {
+      appNotifications.markAsReadDesktop(event, $(this));
+    });
+  },
+  loadAll: () => {
+    appNotifications.loadNumber();
+  },
+  badgeLoadingMask: function (show) {
+    if (show === true) {
+      $("#notificationsBadge").html(appNotifications.badgeSpinner);
+      $("#notificationsBadge").show();
+      // Mobile
+      $("#notificationsBadgeMobile").html(count);
+      $("#notificationsBadgeMobile").show();
+    }
+    else {
+      $("#notificationsBadge").html(count);
+      if (count > 0) {
+        $("#notificationsIcon").removeClass("fa-bell-o");
+        $("#notificationsIcon").addClass("fa-bell");
+        $("#notificationsBadge").show();
+        // Mobile
+        $("#notificationsIconMobile").removeClass("fa-bell-o");
+        $("#notificationsIconMobile").addClass("fa-bell");
+        $("#notificationsBadgeMobile").show();
+      }
+      else {
+        $("#notificationsIcon").addClass("fa-bell-o");
+        $("#notificationsBadge").hide();
+        // Mobile
+        $("#notificationsIconMobile").addClass("fa-bell-o");
+        $("#notificationsBadgeMobile").hide();
+      }
+    }
+  },
+  loadingMask: function (show) {
+    if (show === true) {
+      $("#notificationAucune").hide();
+      $("#notificationsLoader").show();
+    } else {
+      $("#notificationsLoader").hide();
+      if (count > 0) {
+        $("#notificationAucune").hide();
+      }
+      else {
+        $("#notificationAucune").show();
+      }
+    }
+  },
+  loadNumber: () => {
+    appNotifications.badgeLoadingMask(true);
+
+    $.get('/api/user/notifications', data => {
+      count = data.count;
+      $("#notificationsBadge").html(data.count);
+      appNotifications.badgeLoadingMask(false);
+      appNotifications.load(data.rows);
+    });
+  },
+  load: (notifications) => {
+    appNotifications.loadingMask(true);
+
+    $('#notificationsContainer').html('');
+
+    lastCount = count;
+
+    notifications.forEach(notif => {
+      let template = $('#notificationTemplate').html();
+      template = template.replace("{{id}}", notif.id);
+      template = template.replace("{{image}}", notif.image);
+      template = template.replace("{{subject}}", notif.subject);
+      template = template.replace("{{title}}", notif.title);
+      template = template.replace("{{date}}", moment(notif.createdAt).startOf('day').fromNow());
+      $('#notificationsContainer').append(template);
+    });
+    // bind mark as read
+    $('.notification-read').on('click', (event) => appNotifications.markAsRead(event, $(this)));
+
+    // Stop loading mask
+    appNotifications.loadingMask(false);
+
+    // enable button
+    $("#notifications-dropdown").prop("disabled", false);
+  },
+  // Set notification as read
+  markAsRead: (event) => {
+    // Allow to keep list open
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Remove notification
+    let elem = $(event.currentTarget).parent('.dropdown-notification');
+    let id = elem.attr('data-notif-id');
+    elem.remove();
+    $.put('/api/user/notification/read', { _csrf, id }, (data) => {
+      console.log(data);
+      count--;
+      appNotifications.loadAll();
+    });
+  },
+  // Marquer une notification comme lue version bureau
+  markAsReadDesktop: function (event, elem) {
+    // Permet de ne pas change de page
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Suppression de la notification
+    elem.parent('.dropdown-notification').removeClass("notification-unread");
+    elem.remove();
+
+    // On supprime le focus
+    if (document.activeElement) {
+      document.activeElement.blur();
+    }
+
+    count--;
+
+    // update count
+    appNotifications.loadAll();
+  },
+  badgeSpinner: '<i class="fal fa-spinner fa-pulse fa-fw" aria-hidden="true"></i>'
+};
