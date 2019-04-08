04557d198172cb9713da2519104965828a7b4532
Kronox21 <piccolo.rom@gmail.com>
2019-04-08 22:52:24 +0200

    input type date, need to find why the date is not visible in the input

--- src/javascripts/ats.js
+++ src/javascripts/ats.js
@@ -182,21 +182,21 @@ let contractModalListener = () => {
       $('#serviceModal').modal('show');
   });
 
-  $('.from').datepicker();
-  $('.from').on('dp.change', (e) => {
-    if (!e.date)
-      delete application.start;
-    else
-      application.start = new Date(e.date);
-  });
-
-  $('.to').datepicker();
-  $('.to').on('dp.change', (e) => {
-    if (!e.date)
-      delete application.end;
-    else
-      application.end = new Date(e.date);
-  });
+  // $('.from').datepicker();
+  // $('.from').on('dp.change', (e) => {
+  //   if (!e.date)
+  //     delete application.start;
+  //   else
+  //     application.start = new Date(e.date);
+  // });
+  //
+  // $('.to').datepicker();
+  // $('.to').on('dp.change', (e) => {
+  //   if (!e.date)
+  //     delete application.end;
+  //   else
+  //     application.end = new Date(e.date);
+  // });
 };
 
 $(document).ready(function () {
