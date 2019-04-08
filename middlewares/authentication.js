f0bd1a44a77925b116d4acc7476f3eddeab5a9b0
Сорок два <garcia.llyam@gmail.com>
2019-04-08 16:21:33 +0200

    throw BackError 403 on authentication middlewares

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,5 +1,7 @@
-const Models = require('../orm/models');
 const _ = require('lodash');
+const httpsStatus = require('http-status');
+const { BackError } = require(`../helpers/back.error`);
+const Models = require('../orm/models');
 const Authentication = {};
 
 /**
@@ -31,7 +33,7 @@ Authentication.ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   } else {
-    req.flash('error_msg', 'You are not logged');
+    req.flash('error_msg', 'Vous n\'êtes pas connecté.');
     res.redirect('/');
   }
 };
@@ -49,10 +51,11 @@ Authentication.ensureIsAdmin = (req, res, next) => {
     if (['Admin'].includes(req.user.role) || ['Admin'].includes(req.session.role)) {
       next();
     } else {
-      res.redirect('/');
+      console.log(req.user, req.session);
+      return next(new BackError('Vous n\'avez pas accès à cette page.', httpsStatus.FORBIDDEN));
     }
   } else {
-    res.redirect('/');
+    return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
   }
 };
 
@@ -69,10 +72,10 @@ Authentication.ensureIsCandidate = (req, res, next) => {
     if (['candidate'].includes(req.user.type)) {
       next();
     } else {
-      res.redirect('/');
+      return next(new BackError('Vous n\'avez pas accès à cette page.', httpsStatus.FORBIDDEN));
     }
   } else {
-    res.redirect('/');
+    return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
   }
 };
 
@@ -90,10 +93,10 @@ Authentication.ensureIsEs = (req, res, next) => {
       if (_.isNil(req.session.currentEs) && req.url.search('/select/es') === -1) return res.redirect('/select/es');
       next();
     } else {
-      res.redirect('/');
+      return next(new BackError('Vous n\'avez pas accès à cette page.', httpsStatus.FORBIDDEN));
     }
   } else {
-    res.redirect('/');
+    return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
   }
 };
 
