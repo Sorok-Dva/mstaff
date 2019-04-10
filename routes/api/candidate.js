7bfd16b050f8ac134f48e9d289ff74480db647a0
Сорок два <garcia.llyam@gmail.com>
2019-04-10 17:09:44 +0200

    add conferences related routes

--- routes/api/candidate.js
+++ routes/api/candidate.js
@@ -1,6 +1,6 @@
 const __ = process.cwd();
 const { Authentication, HTTPValidation } = require(`${__}/middlewares/index`);
-const { User, Establishment } = require(`${__}/components`);
+const { User, Establishment, Conference } = require(`${__}/components`);
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
@@ -115,6 +115,11 @@ router.get('/wish/:id(\\d+)',
 router.post('/nc/:id(\\d+)/availability',
   Authentication.ensureIsCandidate,
   Establishment.Need.candidateAnswer
+);
+
+router.post('/conference/:id(\\d+)/availability',
+  Authentication.ensureIsCandidate,
+  Conference.Main.candidateAnswer
 );
 
 module.exports = router;
