04a13f515f6fb10f0367c81f186bfb5442bfc5d4
Сорок два <garcia.llyam@gmail.com>
2019-04-15 14:15:24 +0200

    get maintenance param value and return it

--- components/server/server.js
+++ components/server/server.js
@@ -1,8 +1,15 @@
-const Models = require('../../orm/models');
+const __ = process.cwd();
+const { BackError } = require(`${__}/helpers/back.error`);
+const { Op } = require('sequelize');
+const _ = require('lodash');
+const Models = require(`${__}/orm/models/index`);
 
 module.exports = {
   verifyMaintenance: callback => {
-    // @todo create server table and take value from it
-    return callback('live');
+    Models.ServerParameter.findOne({ where: { param: 'maintenance' } }).then(param => {
+      if (_.isNil(param)) return callback('live');
+      if (_.isNil(param.value)) return callback('live');
+      return callback(param.value);
+    });
   }
 };
