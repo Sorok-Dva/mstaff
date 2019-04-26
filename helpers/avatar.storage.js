3bdb5c346ac1dc33865cc3dcb0b9490909633f2a
Luc~ <Luc@mstaff.co>
2019-04-26 16:03:12 +0200

    Adding AvatarStorage function and ES6 reform

--- helpers/avatar.storage.js
+++ helpers/avatar.storage.js
@@ -1,28 +1,93 @@
-var _ = require('lodash');
-var fs = require('fs');
-var path = require('path');
-var Jimp = require('jimp');
-var crypto = require('crypto');
-var mkdirp = require('mkdirp');
-var concat = require('concat-stream');
-var streamifier = require('streamifier');
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const Jimp = require('jimp');
+const crypto = require('crypto');
+const mkdirp = require('mkdirp');
+const concat = require('concat-stream');
+const streamifier = require('streamifier');
 
-var UPLOAD_PATH = path.resolve(__dirname, '..', process.env.AVATAR_STORAGE);
+const UPLOAD_PATH = path.resolve(__dirname, '..', process.env.AVATAR_STORAGE);
 
 
-var AvatarStorage = function(options) {
+let AvatarStorage = function (options) {
 
-  function AvatarStorage(opts) {}
+  function AvatarStorage(opts) {
+    let baseUrl = process.env.AVATAR_BASE_URL;
 
-  AvatarStorage.prototype._generateRandomFilename = function () {};
+    const allowedStorageSystems = ['local'];
+    const allowedOutputFormats = ['jpg', 'png'];
 
-  AvatarStorage.prototype._createOutputStream = function (filepath, cb) {};
+    const defaultOptions = {
+      storage: 'local',
+      output: 'png',
+      greyscale: false,
+      quality: 70,
+      square: true,
+      threshold: 500,
+      responsive: false,
+    };
 
-  AvatarStorage.prototype._processImage = function (image, cb) {};
+    let options = (opts && _.isObject(opts)) ? _.pick(opts, _.keys(defaultOptions)) : {};
+    options = _.extend(defaultOptions, options);
 
-  AvatarStorage.prototype._handleFile = function (req, file, cb) {};
+    this.options = _.forIn(options, function (value, key, object) {
 
-  AvatarStorage.prototype._removeFile = function (req, file, cb) {};
+      switch (key) {
+
+        case 'square':
+        case 'greyscale':
+        case 'responsive':
+          object[key] = _.isBoolean(value) ? value : defaultOptions[key];
+          break;
+
+        case 'storage':
+          value = String(value).toLowerCase();
+          object[key] = _.includes(allowedStorageSystems, value) ? value : defaultOptions[key];
+          break;
+
+        case 'output':
+          value = String(value).toLowerCase();
+          object[key] = _.includes(allowedOutputFormats, value) ? value : defaultOptions[key];
+          break;
+
+        case 'quality':
+          value = _.isFinite(value) ? value : Number(value);
+          object[key] = (value && value >= 0 && value <= 100) ? value : defaultOptions[key];
+          break;
+
+        case 'threshold':
+          value = _.isFinite(value) ? value : Number(value);
+          object[key] = (value && value >= 0) ? value : defaultOptions[key];
+          break;
+
+      }
+
+    });
+
+    this.uploadPath = this.options.responsive ? path.join(UPLOAD_PATH, 'responsive') : UPLOAD_PATH;
+
+    this.uploadBaseUrl = this.options.responsive ? path.join(baseUrl, 'responsive') : baseUrl;
+
+    if (this.options.storage == 'local') {
+      !fs.existsSync(this.uploadPath) && mkdirp.sync(this.uploadPath);
+    }
+  }
+
+  AvatarStorage.prototype._generateRandomFilename = function () {
+  };
+
+  AvatarStorage.prototype._createOutputStream = function (filepath, cb) {
+  };
+
+  AvatarStorage.prototype._processImage = function (image, cb) {
+  };
+
+  AvatarStorage.prototype._handleFile = function (req, file, cb) {
+  };
+
+  AvatarStorage.prototype._removeFile = function (req, file, cb) {
+  };
 
   return new AvatarStorage(options);
 
