2416fcfcfc6181323a4b14e88de70a0a30389061
Luc~ <Luc@mstaff.co>
2019-05-01 12:57:34 +0200

    Database ok Fixing toggle

--- orm/migrations/20190430164399-create-mstaff-category.js
+++ orm/migrations/20190430164399-create-mstaff-category.js
@@ -13,6 +13,38 @@ module.exports = {
         primaryKey: true,
         unique: true
       },
+      medical: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      paramedical: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      admin_and_tech: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      liberal: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      cdi_cdd: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      vacation: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      internship: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      service: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
--- orm/models/mstaffcategory.js
+++ orm/models/mstaffcategory.js
@@ -2,6 +2,14 @@
 module.exports = (sequelize, DataTypes) => {
   const MstaffCategory = sequelize.define('MstaffCategories', {
     name: DataTypes.STRING,
+    medical: DataTypes.BOOLEAN,
+    paramedical: DataTypes.BOOLEAN,
+    admin_and_tech: DataTypes.BOOLEAN,
+    liberal: DataTypes.BOOLEAN,
+    cdi_cdd: DataTypes.BOOLEAN,
+    vacation: DataTypes.BOOLEAN,
+    internship: DataTypes.BOOLEAN,
+    service: DataTypes.BOOLEAN
   }, {});
   MstaffCategory.associate = function (models) {
 
