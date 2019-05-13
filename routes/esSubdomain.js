d673ffbb39cffe08189a96addbf648596ca4d502
Сорок два <garcia.llyam@gmail.com>
2019-05-13 18:35:48 +0200

    update esSubdomain route with ats branch

--- routes/esSubdomain.js
+++ routes/esSubdomain.js
@@ -1,13 +1,48 @@
 const { Authentication } = require('../middlewares/index');
-const { Establishment } = require('../components');
+const { Establishment, User } = require('../components');
+const { HTTPValidation } = require('../middlewares/');
 const express = require('express');
 const router = express.Router();
 
 router.get('/',
-  Establishment.Website.getIndex);
+  Establishment.Website.ViewIndex);
+
+router.get('/join',
+  Establishment.Website.ViewATS);
 
 router.get('/register',
   Authentication.ensureIsNotAuthenticated,
-  Establishment.Website.getRegister);
+  Establishment.Website.ViewRegister)
+  .post('/register',
+    Authentication.ensureIsNotAuthenticated,
+    HTTPValidation.UserController.create,
+    User.Main.create);
+
+router.post('/add/diploma',
+  Authentication.ensureIsNotAuthenticated,
+  HTTPValidation.CandidateController.postAddDiploma,
+  User.Candidate.AddDiploma
+);
+
+router.post('/add/experience',
+  //TODO verifier user id si pas d authentification
+  // Authentication.ensureIsNotAuthenticated,
+  HTTPValidation.CandidateController.postAddExperience,
+  User.Candidate.AddExperience
+);
+
+router.get('/emailAvailable/:email',
+  HTTPValidation.UserController.ApiVerifyEmailAvailability,
+  User.Main.verifyEmailAvailability);
+
+router.get('/posts/all',
+  Establishment.Website.GetPosts);
+
+router.get('/services/all',
+  Establishment.Website.GetServices);
+
+router.get('/atsDatas/all',
+  Establishment.Website.GetAtsDatas);
+
 
 module.exports = router;
