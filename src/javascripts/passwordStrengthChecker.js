2a9980a23f1ec8cfd2a6a5ca55e8bead90188a81
Сорок два <garcia.llyam@gmail.com>
2019-05-20 00:37:07 +0200

    fix passwordStrengthChecker

--- src/javascripts/passwordStrengthChecker.js
+++ src/javascripts/passwordStrengthChecker.js
@@ -11,6 +11,9 @@ let ValidatePassword = function () {
   }, {
     Pattern: '[!@#$%^&*]',
     Target: 'symbol'
+  }, {
+    Pattern: ($('#passwordConfirm').val() === $('#password').val()),
+    Target: 'same'
   }];
 
   let password = $(this).val();
@@ -18,13 +21,25 @@ let ValidatePassword = function () {
   $('#length').removeClass(password.length > 8 ? 'bad-rule' : 'good-rule');
   $('#length').addClass(password.length > 8 ? 'good-rule' : 'bad-rule');
 
+  if ($('#passwordConfirm').val() === password) {
+    $('#password').removeClass('is-invalid').addClass('is-valid');
+    ('#same').removeClass('bad-rule').addClass('good-rule');
+  } else {
+    $('#password').removeClass('is-valid').addClass('is-invalid');
+    $('#same').removeClass('good-rule').addClass('bad-rule');
+    $('button#validateForm').attr('disabled', 'disabled');
+  }
   for (let i = 0; i < rules.length; i++) {
-    $('#' + rules[i].Target).removeClass(new RegExp(rules[i].Pattern).test(password) ? 'bad-rule' : 'good-rule');
-    $('#' + rules[i].Target).addClass(new RegExp(rules[i].Pattern).test(password) ? 'good-rule' : 'bad-rule');
+    $('#' + rules[i].Target)
+      .removeClass(new RegExp(rules[i].Pattern).test(password) ? 'bad-rule' : 'good-rule')
+      .addClass(new RegExp(rules[i].Pattern).test(password) ? 'good-rule' : 'bad-rule');
   }
   if ($('ul').find('.bad-rule').length === 0) {
     $('button#validateForm').removeAttr('disabled');
     $('#password').removeClass('is-invalid').addClass('is-valid');
+  } else if ($('ul').find('.bad-rule').length === 1 && $('#same').hasClass('bad-rule')) {
+    $('#password').removeClass('is-invalid').addClass('is-valid');
+    $('button#validateForm').attr('disabled', 'disabled');
   } else {
     $('button#validateForm').attr('disabled', 'disabled');
     $('#password').removeClass('is-valid').addClass('is-invalid');
@@ -40,16 +55,13 @@ let ValidateConfirmPassword = function () {
   }];
 
   if (rules[0].Pattern) {
-    $('#' + rules[0].Target).removeClass('bad-rule');
-    $('#' + rules[0].Target).addClass('good-rule');
+    $('#' + rules[0].Target).removeClass('bad-rule').addClass('good-rule');
+    $(this).removeClass('is-invalid').addClass('is-valid');
+    $('button#validateForm').removeAttr('disabled');
   } else {
-    $('#' + rules[0].Target).removeClass('good-rule');
-    $('#' + rules[0].Target).addClass('bad-rule');
-  }
-  if ($('ul').find('.bad-rule').length === 0) {
-    $('button#resetPassword').removeAttr('disabled');
-  } else {
-    $('button#resetPassword').attr('disabled', 'disabled');
+    $('#' + rules[0].Target).removeClass('good-rule').addClass('bad-rule');
+    $(this).removeClass('is-valid').addClass('is-invalid');
+    $('button#validateForm').attr('disabled', 'disabled');
   }
 };
 
