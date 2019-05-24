beccbae42039905711ed40b8355a3b98e7c5336a
Сорок два <garcia.llyam@gmail.com>
2019-05-24 14:23:00 +0200

    improve authent middleware + req.logout and session.destroy is req.user is nil

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -47,14 +47,15 @@ Authentication.ensureAuthenticated = (req, res, next) => {
  * @description Ensure that the current user is an admin
  */
 Authentication.ensureIsAdmin = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    if (_.isNil(req.user)) return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
+  if (req.isAuthenticated() && !_.isNil(req.user)) {
     if (['Admin'].includes(req.user.role) || ['Admin'].includes(req.session.role)) {
       next();
     } else {
       return next(new BackError('Vous n\'avez pas accès à cette page.', httpsStatus.FORBIDDEN));
     }
   } else {
+    req.logout();
+    req.session.destroy();
     return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
   }
 };
@@ -68,14 +69,15 @@ Authentication.ensureIsAdmin = (req, res, next) => {
  * @description Ensure that the current user is a candidate
  */
 Authentication.ensureIsCandidate = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    if (_.isNil(req.user)) return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
+  if (req.isAuthenticated() && !_.isNil(req.user)) {
     if (['candidate'].includes(req.user.type)) {
       next();
     } else {
       return next(new BackError('Vous n\'avez pas accès à cette page.', httpsStatus.FORBIDDEN));
     }
   } else {
+    req.logout();
+    req.session.destroy();
     return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
   }
 };
@@ -89,8 +91,7 @@ Authentication.ensureIsCandidate = (req, res, next) => {
  * @description Ensure that the current user is an es
  */
 Authentication.ensureIsEs = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    if (_.isNil(req.user)) return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
+  if (req.isAuthenticated() && !_.isNil(req.user)) {
     if (['es'].includes(req.user.type)) {
       if ((_.isNil(req.user.opts) || !('currentEs' in req.user.opts)) && req.url.search('/select/es') === -1) return res.redirect('/select/es');
       next();
@@ -98,6 +99,8 @@ Authentication.ensureIsEs = (req, res, next) => {
       return next(new BackError('Vous n\'avez pas accès à cette page.', httpsStatus.FORBIDDEN));
     }
   } else {
+    req.logout();
+    req.session.destroy();
     return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
   }
 };
@@ -110,7 +113,11 @@ Authentication.ensureIsEs = (req, res, next) => {
  * @description ensure that the current user belongs to the establishment call in the route.
  */
 Authentication.verifyEsAccess = (req, res, next) => {
-  if (_.isNil(req.user)) return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
+  if (_.isNil(req.user)) {
+    req.logout();
+    req.session.destroy();
+    return next(new BackError('Vous devez être connecté.', httpsStatus.FORBIDDEN));
+  }
   Models.Establishment.findOne({
     where: { id: req.params.esId || req.user.opts.currentEs },
     include: {
