fe5a1562baf1c52b2ffbb0030354d4ddc2ec0adf
Kronox21 <piccolo.rom@gmail.com>
2019-05-31 16:24:16 +0200

    invitationATS model

---
+++ orm/models/invitationats.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const InvitationAts = sequelize.define('InvitationAts', {
+    key: DataTypes.STRING,
+    es: DataTypes.JSON
+  }, {});
+  InvitationAts.associate = function(models) {
+    // associations can be defined here
+  };
+  return InvitationAts;
+};
