51d11e8db6173cdcf2e2ccc7e4c9d741533d69ad
Kronox21 <piccolo.rom@gmail.com>
2019-06-06 19:51:14 +0200

    group find

--- components/subdomain/group/group.controller.js
+++ components/subdomain/group/group.controller.js
@@ -15,16 +15,16 @@ Subdomain_Group.ViewIndex = (req, res, next) => {
 Subdomain_Group.find = (id, next) => {
   return Models.Groups.findOne({
     where: { id },
-    include: {
+    include: [{
       model: Models.EstablishmentGroups,
-      include: {
+      include: [{
         model: Models.Establishment,
         as: 'es',
         on: {
           'id': {
             [Op.col]: 'id_es'
           }
-        }
+        },
         /*include: {
           model: Models.EstablishmentReference,
           as: 'ref',
@@ -35,8 +35,8 @@ Subdomain_Group.find = (id, next) => {
           },
           attributes: ['lat', 'lon', 'finess_et']
         }*/
-      }
-    }
+      }]
+    }]
   }).then( group => {
     group.es = [];
     group.EstablishmentGroups.forEach( item => {
