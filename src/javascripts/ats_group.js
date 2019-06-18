153163df2f7082747144f26dea690a2f77bea605
Kronox21 <piccolo.rom@gmail.com>
2019-06-18 11:35:42 +0200

    RM-188

--- src/javascripts/ats_group.js
+++ src/javascripts/ats_group.js
@@ -19,15 +19,48 @@ function groupListener(){
       }
     }
   });
+}
+
+function notify(error){
+  switch (error) {
+    case 'noEstablishment':
+      notification({
+        icon: 'exclamation',
+        type: 'danger',
+        title: 'Informations manquantes :',
+        message: `Merci de sélectionner au minimum un établissement.`
+      });
+      break;
+  }
+  return false;
 };
 
 function saveDatas(){
-};
+  let checked = $('#finessList input:checked');
+  let finessList = [];
+
+  Object.keys(checked).forEach( key => {
+    let id = checked[key].id;
+    if (!isNaN(key)){
+      if (!isNaN(id))
+        finessList.push(id);
+    }
+  });
+  candidateDatas.finess = finessList;
+}
 
 function verifyInputs(){
-  return $('#finessList input:checked').length > 0;
-};
+  return $('#finessList input:checked').length > 0 ? true : notify('noEstablishment');;
+}
+
+function reload_group(){
+  candidateDatas.finess.forEach( finess => {
+    $(`#${finess}`).prop('checked', true);
+  });
+}
 
 $(document).ready(() => {
   groupListener();
+  if(candidateDatas.finess.length > 0)
+    reload_group();
 });
