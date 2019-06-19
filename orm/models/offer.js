232c61619d1b19c31acf992e3189a9170dbe847f
Сорок два <garcia.llyam@gmail.com>
2019-06-19 16:26:17 +0200

    fix Offer JSON getters

--- orm/models/offer.js
+++ orm/models/offer.js
@@ -9,7 +9,8 @@ module.exports = (sequelize, DataTypes) => {
     nature_section: {
       type: DataTypes.JSON,
       get() {
-        return JSON.parse(this.getDataValue('nature_section'))
+        let nature_section = this.getDataValue('nature_section') === undefined ? '{}' : this.getDataValue('nature_section');
+        return JSON.parse(nature_section);
       },
       set(data) {
         this.setDataValue('nature_section', JSON.stringify(data));
@@ -18,7 +19,8 @@ module.exports = (sequelize, DataTypes) => {
     context_section: {
       type: DataTypes.JSON,
       get() {
-        return JSON.parse(this.getDataValue('context_section'))
+        let context_section = this.getDataValue('context_section') === undefined ? '{}' : this.getDataValue('context_section');
+        return JSON.parse(context_section);
       },
       set(data) {
         this.setDataValue('context_section', JSON.stringify(data));
@@ -27,7 +29,8 @@ module.exports = (sequelize, DataTypes) => {
     details_section: {
       type: DataTypes.JSON,
       get() {
-        return JSON.parse(this.getDataValue('details_section'))
+        let details_section = this.getDataValue('details_section') === undefined ? '{}' : this.getDataValue('details_section');
+        return JSON.parse(details_section);
       },
       set(data) {
         this.setDataValue('details_section', JSON.stringify(data));
@@ -36,7 +39,9 @@ module.exports = (sequelize, DataTypes) => {
     postDescription_section: {
       type: DataTypes.JSON,
       get() {
-        return JSON.parse(this.getDataValue('postDescription_section'))
+        let postDescription_section = this.getDataValue('postDescription_section') === undefined ? '{}'
+          : this.getDataValue('postDescription_section');
+        return JSON.parse(postDescription_section);
       },
       set(data) {
         this.setDataValue('postDescription_section', JSON.stringify(data));
@@ -45,7 +50,9 @@ module.exports = (sequelize, DataTypes) => {
     prerequisites_section: {
       type: DataTypes.JSON,
       get() {
-        return JSON.parse(this.getDataValue('prerequisites_section'))
+        let prerequisites_section = this.getDataValue('prerequisites_section') === undefined ? '{}'
+          : this.getDataValue('prerequisites_section');
+        return JSON.parse(prerequisites_section);
       },
       set(data) {
         this.setDataValue('prerequisites_section', JSON.stringify(data));
@@ -54,7 +61,8 @@ module.exports = (sequelize, DataTypes) => {
     terms_sections: {
       type: DataTypes.JSON,
       get() {
-        return JSON.parse(this.getDataValue('terms_sections'))
+        let terms_sections = this.getDataValue('terms_sections') === undefined ? '{}' : this.getDataValue('terms_sections');
+        return JSON.parse(terms_sections);
       },
       set(data) {
         this.setDataValue('terms_sections', JSON.stringify(data));
