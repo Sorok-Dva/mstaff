4f291673aa0c56f6eeb6d5459c18cd406fde4ecc
Сорок два <garcia.llyam@gmail.com>
2019-06-19 15:59:34 +0200

    add establishment offer component

--- components/establishment/offer/offer.controller.js
+++ components/establishment/offer/offer.controller.js
@@ -11,463 +11,100 @@ const mailer = require(`${__}/bin/mailer`);
 const Models = require(`${__}/orm/models/index`);
 const Mailer = require(`${__}/components/mailer`);
 
-const Establishment_Need = {};
+const Establishment_Offer = {};
 
-Establishment_Need.ViewAll = (req, res, next) => {
-  Models.Need.findAll({
-    where: { es_id: req.user.opts.currentEs, closed: false },
+Establishment_Offer.ViewAll = (req, res, next) => {
+  Models.Offer.findAll({
+    where: { es_id: req.user.opts.currentEs },
     order: [['createdAt', 'DESC']],
-    include: [{
-      model: Models.NeedCandidate,
-      as: 'candidates',
-      required: true
-    }, {
-      model: Models.User,
-    }]
-  }).then(needs => {
-    res.render('establishments/needs', { needs, a: { main: 'needs' } });
+  }).then(offers => {
+    res.render('establishments/offers', { offers, a: { main: 'offers' } });
   }).catch(error => next(new BackError(error)));
 };
 
-Establishment_Need.ViewClosed = (req, res, next) => {
-  Models.Need.findAll({
-    where: { es_id: req.user.opts.currentEs, closed: true },
-    include: [{
-      model: Models.NeedCandidate,
-      as: 'candidates',
-      required: true
-    }, {
-      model: Models.User,
-    }]
-  }).then(needs => {
-    res.render('establishments/needs_closed', { needs, a: { main: 'history' } });
-  }).catch(error => next(new BackError(error)));
-};
-
-Establishment_Need.View = (req, res, next) => {
-  let render = { a: { main: 'needs' } };
-  Models.Need.findOne({
-    where: { id: req.params.id, es_id: req.user.opts.currentEs, closed: false },
-    include: [{
-      model: Models.NeedCandidate,
-      as: 'candidates',
-      required: true,
-      include: {
-        model: Models.Candidate,
-        required: true,
-        include: [{
-          model: Models.User,
-          attributes: ['id', 'firstName', 'lastName', 'birthday'],
-          on: {
-            '$candidates->Candidate.user_id$': {
-              [Op.col]: 'candidates->Candidate->User.id'
-            }
-          },
-          required: true
-        }, {
-          model: Models.Application,
-          attributes: ['id', 'wish_id', 'candidate_id'],
-          as: 'applications',
-          on: {
-            '$candidates->Candidate.id$': {
-              [Op.col]: 'candidates->Candidate->applications.candidate_id'
-            }
-          },
-          include: {
-            model: Models.Wish,
-            on: {
-              '$candidates->Candidate.id$': {
-                [Op.col]: 'candidates->Candidate->applications->Wish.candidate_id'
-              }
-            },
-          },
-          required: true
-        }]
-      }
-    }, {
+Establishment_Offer.View = (req, res, next) => {
+  let render = { a: { main: 'offers' } };
+  Models.Offer.findOne({
+    where: { id: req.params.id, es_id: req.user.opts.currentEs },
+    include: {
       model: Models.Establishment,
       required: true
-    }]
-  }).then(need => {
-    if (_.isNil(need)) return next(new BackError(`Besoin ${req.params.id} introuvable.`, httpStatus.NOT_FOUND));
-    render.need = need;
-    return res.render('establishments/showNeed', render);
+    }
+  }).then(offer => {
+    if (_.isNil(offer)) return next(new BackError(`Offre ${req.params.id} introuvable.`, httpStatus.NOT_FOUND));
+    render.offer = offer;
+    return res.render('establishments/showOffer', render);
   }).catch(error => next(new BackError(error)));
 };
 
-Establishment_Need.ViewHistory = (req, res, next) => {
-  let render = { a: { main: 'history' } };
-  Models.Need.findOne({
-    where: { id: req.params.id, closed: true },
-    include: [{
-      model: Models.NeedCandidate,
-      as: 'candidates',
-      required: true,
-      include: {
-        model: Models.Candidate,
-        required: false,
-        include: [{
-          model: Models.User,
-          attributes: ['id', 'firstName', 'lastName', 'birthday'],
-          on: {
-            '$candidates->Candidate.user_id$': {
-              [Op.col]: 'candidates->Candidate->User.id'
-            }
-          },
-          required: false
-        }, {
-          model: Models.Application,
-          attributes: ['id', 'wish_id', 'candidate_id'],
-          as: 'applications',
-          on: {
-            '$candidates->Candidate.id$': {
-              [Op.col]: 'candidates->Candidate->applications.candidate_id'
-            }
-          },
-          include: {
-            model: Models.Wish,
-            on: {
-              '$candidates->Candidate.id$': {
-                [Op.col]: 'candidates->Candidate->applications->Wish.candidate_id'
-              }
-            },
-          },
-          required: true
-        }]
-      }
-    }, {
-      model: Models.Establishment,
-      required: true
-    }]
-  }).then(need => {
-    if (_.isNil(need)) return next(new BackError(`Besoin ${req.params.id} introuvable.`, httpStatus.NOT_FOUND));
-    render.need = need;
-    return res.render('establishments/showNeedClosed', render);
-  }).catch(error => next(new BackError(error)));
-};
-
-Establishment_Need.edit = (req, res, next) => {
+Establishment_Offer.edit = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).send({ body: req.body, errors: errors.array() });
   }
-  Models.Need.update({
-    name: req.body.name || 'Besoin sans nom',
-    contract_type: !_.isNil(req.body.filterQuery.contractType) ? req.body.filterQuery.contractType : null,
-    post: !_.isNil(req.body.post) ? req.body.post || null : null,
-    service: !_.isNil(req.body.filterQuery.service) ? req.body.filterQuery.service || null : null,
-    diploma: !_.isNil(req.body.filterQuery.diploma) ? req.body.filterQuery.diploma || null : null,
-    is_available: !_.isNil(req.body.filterQuery.is_available) ? req.body.filterQuery.is_available || null : null,
-    postal_code: !_.isNil(req.body.filterQuery.postal_code) ? req.body.filterQuery.postal_code || null : null,
-    start: !_.isNil(req.body.filterQuery.timeType) ? req.body.filterQuery.timeType.dateStart || null : null,
-    end: !_.isNil(req.body.filterQuery.timeType) ? req.body.filterQuery.timeType.dateEnd || null : null
+  Models.Offer.update({
+    name: req.body.name || 'Offre sans nom',
   }, {
     where: { id: req.body.id }
-  }).then(need => {
-    req.flash('success_msg', `Besoin modifié avec succès.`);
-    if (!_.isNil(req.body.selectedCandidates)) {
-      req.body.selectedCandidates = JSON.parse(`[${req.body.selectedCandidates}]`);
-
-      for (let i = 0; i < req.body.selectedCandidates.length; i++) {
-        Models.NeedCandidate.findOrCreate({
-          where: {
-            need_id: req.body.id,
-            candidate_id: req.body.selectedCandidates[i],
-          },
-          defaults: {
-            notified: req.body.notifyCandidates,
-            status: req.body.notifyCandidates === 'true' ? 'notified' : 'pre-selected'
-          }
-        }).spread((needCandidate, created) => {
-          if (created) {
-            if (req.body.notifyCandidates === 'true') {
-              Establishment_Need.notify(req, i, needCandidate, need);
-            }
-          }
-        });
-      }
-    }
-    res.status(201).send({ need, status: 'updated' });
+  }).then(offer => {
+    req.flash('success_msg', `Offre modifiée avec succès.`);
+    res.status(201).send({ offer, status: 'updated' });
   });
 };
 
-Establishment_Need.delete = (req, res, next) => {
+Establishment_Offer.delete = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).send({ body: req.body, errors: errors.array() });
   }
-  Models.Need.findOne({
+  Models.Offer.findOne({
     where: { id: req.params.id, createdBy: req.user.id }
-  }).then(need => {
-    if (_.isNil(need)) return next(new BackError('Besoin introuvable.', 404));
-    return need.destroy().then(data => res.status(201).send({ deleted: true, data }));
+  }).then(offer => {
+    if (_.isNil(offer)) return next(new BackError('Offre introuvable.', 404));
+    return offer.destroy().then(data => res.status(201).send({ deleted: true, data }));
   }).catch(error => new BackError(error));
 };
 
-Establishment_Need.Create = (req, res, next) => {
+Establishment_Offer.Create = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).send({ body: req.body, errors: errors.array() });
   }
-  Models.Need.create({
-    name: req.body.name || 'Besoin sans nom',
-    es_id: req.params.esId,
-    contract_type: !_.isNil(req.body.filterQuery.contractType) ? req.body.filterQuery.contractType : null,
-    post: !_.isNil(req.body.post) ? req.body.post : null,
-    service: !_.isNil(req.body.filterQuery.service) ? req.body.filterQuery.service : null,
-    diploma: !_.isNil(req.body.filterQuery.diploma) ? req.body.filterQuery.diploma : null,
-    is_available: !_.isNil(req.body.filterQuery.is_available) ? req.body.filterQuery.is_available : null,
-    postal_code: !_.isNil(req.body.filterQuery.postal_code) ? req.body.filterQuery.postal_code : null,
-    start: !_.isNil(req.body.filterQuery.timeType) ? req.body.filterQuery.timeType.dateStart : null,
-    end: !_.isNil(req.body.filterQuery.timeType) ? req.body.filterQuery.timeType.dateEnd : null,
-    createdBy: req.user.id
-  }).then(need => {
-    req.flash('success_msg', `Besoin ajouté avec succès.`);
-    if (!_.isNil(req.body.selectedCandidates)) {
-      req.body.selectedCandidates = JSON.parse(`[${req.body.selectedCandidates}]`);
-
-      for (let i = 0; i < req.body.selectedCandidates.length; i++) {
-        Models.NeedCandidate.create({
-          need_id: need.id,
-          candidate_id: req.body.selectedCandidates[i],
-          notified: req.body.notifyCandidates,
-          status: req.body.notifyCandidates === 'true' ? 'notified' : 'pre-selected'
-        }).then(needCandidate => {
-          if (req.body.notifyCandidates === 'true') {
-            Establishment_Need.notify(req, i, needCandidate, need);
-          }
-        });
-      }
-    }
-    res.status(201).send(need);
-  });
-};
-
-Establishment_Need.notify = (req, i, needCandidate, need) => {
-  Models.Candidate.findOne({
-    where: { id: req.body.selectedCandidates[i] },
-    include: {
-      model: Models.User,
-      attributes: ['id', 'firstName', 'lastName', 'email'],
-      on: {
-        '$Candidate.user_id$': {
-          [Op.col]: 'User.id'
-        }
-      },
-      required: true
-    }
-  }).then(candidate => {
-    if (_.isNil(candidate)) return false;
-    Models.Notification.create({
-      fromUser: req.user.id,
-      fromEs: req.params.esId,
-      to: candidate.User.id,
-      subject: 'Un établissement est intéressé par votre profil !',
-      title: `Bonne nouvelle !\n L'établissement ${req.es.name} est intéressé par votre profil !`,
-      image: '/static/assets/images/happy.jpg',
-      opts: {
-        type: 'NeedNotifyCandidate',
-        details: {
-          contract: need.contract_type,
-          post: need.post,
-          service: need.service,
-          start: need.start,
-          end: need.end
-        },
-        message: req.body.message,
-        actions: [{
-          'type': 'success',
-          'text': 'Disponible',
-          'dataAttr': `data-ncid="${needCandidate.id}" data-action="nc/availability" data-availability="available"`
-        }, {
-          'type': 'danger',
-          'text': 'Indisponible',
-          'dataAttr': `data-ncid="${needCandidate.id}" data-action="nc/availability" data-availability="unavailable"`
-        }],
-        needCandidateId: needCandidate.id
-      }
-    }).then(notification => {
-      needCandidate.status = 'notified';
-      needCandidate.availability = 'pending';
-      needCandidate.notified = true;
-      needCandidate.save().then(result => {
-        moment.locale('fr');
-        let needObj = {
-          start: _.isNil(need.start) ? null : moment(need.start).format('dddd Do MMMM YYYY'),
-          end: _.isNil(need.end) ? null : moment(need.end).format('dddd Do MMMM YYYY'),
-        };
-        mailer.sendEmail({
-          to: candidate.User.email,
-          subject: 'Un établissement a consulté votre profil.',
-          template: 'candidate/needNotification',
-          context: {
-            needCandidate,
-            needObj: needObj,
-            need,
-            es: req.es
-          }
-        });
-      })
-    });
-  });
-};
-
-Establishment_Need.Feedback = (req, res, next) => {
-  Models.Need.findOne({ where: { id: req.body.needId, es_id: req.params.esId } }).then(need => {
-    if (_.isNil(need)) return next(new BackError(`Besoin ${req.body.needId} introuvable.`, 404));
-    Models.NeedFeedback.create({
-      es_id: req.params.esId,
-      user_id: req.user.id,
-      need_id: need.id,
-      how: req.body.how,
-      stars: req.body.stars || null,
-      feedback: req.body.feedback
-    }).then(feedback => {
-      Establishment_Need.Close(need, req);
-      return res.status(201).send(need);
-    });
-  });
-};
-
-Establishment_Need.Close = (need, req) => {
-  need.closed = true;
-  need.save().then(result => {
-    Models.NeedCandidate.findAll({
-      where: {
-        need_id: need.id,
-        status: ['notified', 'canceled', 'selected']
-      },
-      include: {
-        model: Models.Candidate,
-        attributes: ['user_id'],
-        required: true,
-        include: {
-          model: Models.User,
-          attributes: { exclude: ['password'] },
-          required: true
-        }
-      }
-    }).then(needs => {
-      needs.forEach(need => {
-        let notifObject = {
-          fromUser: req.user.id,
-          fromEs: req.es.id,
-          to: need.Candidate.user_id,
-          opts: {
-            details: {
-              post: result.post,
-              contract: result.contract_type,
-              start: result.start,
-              end: result.end,
-            }
-          }
-        };
-
-        if (need.status === 'notified' || need.status === 'canceled') {
-          notifObject.subject = 'Un établissement a clôturé une offre pour laquelle vous étiez disponible.';
-          notifObject.title = `L'établissement ${req.es.name} a clôturé une offre pour laquelle vous étiez disponible.`;
-          notifObject.image = '/static/assets/images/sad.jpg';
-          notifObject.opts.type = 'NeedNotifyClosedCandidate';
-          Mailer.Main.notifyCandidatesNeedClosed(need.Candidate.User.email, need);
-        }
-        if (need.status === 'selected') {
-          notifObject.subject = 'Vous avez été sélectionné pour l\'offre suivante...';
-          notifObject.title = `L'établissement ${req.es.name} vous a sélectionné pour cette offre dont vous trouverez les détails 
-          ci-dessous et va prendre rapidement contact avec vous.`;
-          notifObject.image = '/static/assets/images/wink.jpg';
-          notifObject.opts.type = 'NeedNotifySelectedCandidate';
-          Mailer.Main.notifyCandidatesNeedSelect(need.Candidate.User.email, need);
-        }
-
-        Models.Notification.create(notifObject);
-      })
-    })
-  });
-};
-
-Establishment_Need.candidateAnswer = (req, res, next) => {
-  Models.Candidate.findOne({ where: { user_id: req.user.id } }).then(candidate => {
-    if (_.isNil(candidate)) return next();
-    Models.NeedCandidate.findOne({
-      where: {
-        id: parseInt(req.params.id),
-        candidate_id: candidate.id
-      },
-      include: {
-        model: Models.Need,
-        on: {
-          '$NeedCandidate.need_id$': {
-            [Op.col]: 'Need.id'
-          }
-        },
-        required: true,
-        include: {
-          model: Models.User,
-          attributes: { exclude: ['password'] },
-          required: true
-        }
-      }
-    }).then(needCandidate => {
-      if (_.isNil(needCandidate)) return next();
-      needCandidate.availability = req.body.availability;
-      needCandidate.save();
-      if (req.body.availability === 'available') {
-        Mailer.Main.notifyESCandidateAvailable(needCandidate.Need.User.email, { Need: needCandidate.Need });
-        Notification.ES.candidateIsAvailableForNeed(needCandidate.Need.User, req.user, needCandidate.Need);
-      }
-      return res.status(200).send('done');
-    })
-  })
-};
-
-Establishment_Need.getNewCandidates = (req, res, next) => {
   Models.Need.findOne({
-    where: { id: req.params.id, es_id: req.params.esId, closed: false },
-    include: [{
-      model: Models.NeedCandidate,
-      as: 'candidates',
-    }, {
+    where: { id: req.params.needId, es_id: req.user.opts.currentEs },
+    include: {
       model: Models.Establishment,
       required: true
-    }]
+    }
   }).then(need => {
-    if (_.isNil(need)) return next(new BackError(`Besoin ${req.params.id} introuvable.`, httpStatus.NOT_FOUND));
-
-    let query = {
-      where: {},
-      include: [{
-        model: Models.Application,
-        where: { es_id: req.params.esId, status: { [Op.not]: 'viewed' } },
-        required: true
-      }, {
-        model: Models.Candidate,
-        attributes: { exclude: ['updatedAt', 'createdAt'] },
-        required: true,
-        include: {
-          model: Models.User,
-          attributes: { exclude: ['password', 'type', 'role', 'email', 'phone', 'updatedAt', 'createdAt'] },
-          on: {
-            '$Candidate.user_id$': {
-              [Op.col]: 'Candidate->User.id'
-            }
-          },
-          required: true
-        }
-      }]
-    };
-
-    if (!_.isNil(need.contract_type)) query.where.contract_type = need.contract_type;
-    if (!_.isNil(need.services)) query.where.services = { [Op.regexp]: Sequelize.literal(`"(${need.service})"`) };
-    if (!_.isNil(need.post)) query.where.posts = { [Op.regexp]: Sequelize.literal(`"(${need.post})"`) };
-
-    Models.Wish.findAll(query).then(wishes => {
-      //remove existing candidates of wishes object if they're in need.needCandidates
-      return res.send(wishes);
+    if (_.isNil(need)) return next(new BackError(`Besoin ${req.params.needId} introuvable.`, httpStatus.NOT_FOUND));
+    Models.Offer.create({
+      name: need.name,
+      need_id: need.id,
+      es_id: need.es_id,
+      nature_section: {
+        contract_type: need.contract_type,
+        start: need.start,
+        end: need.end,
+      },
+      context_section: {
+        place: need.Establishment.name,
+        address: need.Establishment.address + ' ' + need.Establishment.town,
+        website: need.Establishment.url,
+        logo: need.Establishment.logo
+      },
+      status: 'draft',
+      createdBy: req.user.id
+    }).then(offer => {
+      req.flash('success_msg', `Offre ajoutée avec succès.`);
+      res.status(201).send({ status: 'created', offer });
     }).catch(error => next(new BackError(error)));
   }).catch(error => next(new BackError(error)));
+
 };
 
-module.exports = Establishment_Need;
+
+module.exports = Establishment_Offer;
