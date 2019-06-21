b7ca69e08adc8445cf8e34a8d15650553cc6b93b
Kronox21 <piccolo.rom@gmail.com>
2019-06-21 18:56:10 +0200

    contextSection Front OK

---
+++ src/javascripts/job_board_context.js
@@ -0,0 +1,31 @@
+function saveContext(){
+  offerData.context_section.contextLocalisation = $('#contextLocalisation').val();
+  offerData.context_section.contextAddress = $('#contextAddress').val();
+  offerData.context_section.contextAttach = $('#contextAttach').val();
+  offerData.context_section.contextSite = $('#contextSite').val();
+  offerData.context_section.contextPole = $('#contextPole').val();
+  offerData.context_section.contextPresentation = $('#contextPresentation').val();
+  $('.contextSection').remove();
+  load_contextSection();
+}
+
+function cancelContext(){
+  $('.contextSection').remove();
+}
+
+function clearContext(){
+  document.getElementById("contextForm").reset();
+}
+
+function loadContext(){
+  $('#contextLocalisation').val(offerData.context_section.contextLocalisation);
+  $('#contextAddress').val(offerData.context_section.contextAddress);
+  $('#contextAttach').val(offerData.context_section.contextAttach);
+  $('#contextSite').val(offerData.context_section.contextSite);
+  $('#contextPole').val(offerData.context_section.contextPole);
+  $('#contextPresentation').val(offerData.context_section.contextPresentation);
+}
+
+$(document).ready(() => {
+  loadContext();
+});
