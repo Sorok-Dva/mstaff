13c482dc5ffbf73631422e515e2709df6c8327d8
Charles Paris <c.paris93@hotmail.fr>
2019-06-24 12:19:01 +0200

    Adding new model : Users SuperGroups

---
+++ orm/migrations/20190624100302-create-users-super-groups.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('UsersSuperGroups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        },
+        allowNull: false
+      },
+      id_supergroup: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'SuperGroups',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.TEXT,
+        defaultValue: 'User'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('UsersSuperGroups');
+  }
+};
