bc74cbf47e8fe532d6bce2270be9ab90480b41ef
Charles Paris <c.paris93@hotmail.fr>
2019-07-03 17:12:15 +0200

    Creating new model / migration table for poolsInvitation

---
+++ orm/models/invitationpools.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const InvitationPools = sequelize.define('InvitationPools', {
+    email: DataTypes.STRING,
+    token: DataTypes.STRING,
+    pool_id: DataTypes.INTEGER
+  }, {});
+  InvitationPools.associate = function(models) {
+    InvitationPools.belongsTo(models.Pool, {
+      foreignKey: 'id',
+      as: 'pool',
+      onDelete: 'CASCADE'
+    });
+  };
+  return InvitationPools;
+};
