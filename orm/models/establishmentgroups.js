adb2e63f7ed5b2f28a69e5aa975bd17da5b6f537
Kronox21 <piccolo.rom@gmail.com>
2019-09-03 18:35:13 +0200

    modify constraint + delete rows + alter table

--- orm/models/establishmentgroups.js
+++ orm/models/establishmentgroups.js
@@ -1,8 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const EstablishmentGroup = sequelize.define('EstablishmentGroups', {
-    id_es: DataTypes.INTEGER,
-    id_group: DataTypes.INTEGER
+    id_es: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Establishments',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    },
+    id_group: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Groups',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    },
   }, {});
   EstablishmentGroup.associate = function (models) {
     EstablishmentGroup.belongsTo(models.Establishment, {
