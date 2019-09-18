3bf3f4c9baf14d517b13cc6f506b3140aad2cdff
Kronox <piccolo.rom@gmail.com>
2019-09-18 22:29:17 +0200

    rewriting sql migration

--- orm/migrations/20190905160300-alter-establishments.js
+++ orm/migrations/20190905160300-alter-establishments.js
@@ -4,9 +4,11 @@ module.exports = {
     const __ = process.cwd();
     const { Op } = Sequelize;
     const fs = require('fs');
+    const Models = require(`${__}/orm/models/index`);
 
     let sql = '' +
-      /*'DELETE FROM Establishments WHERE name LIKE "%CROIX%ROUGE%";' +
+      'START TRANSACTION;' +
+      'DELETE FROM Establishments WHERE name LIKE "%CROIX%ROUGE%";' +
       'ALTER TABLE Establishments ADD street_number VARCHAR(255);' +
       'ALTER TABLE Establishments ADD city, VARCHAR(255);' +
       'ALTER TABLE Establishments ADD region VARCHAR(255);' +
@@ -18,50 +20,76 @@ module.exports = {
       'ALTER TABLE Establishments ADD attachement_direction VARCHAR(255);' +
       'ALTER TABLE Establishments ADD region_code VARCHAR(255);' +
       'ALTER TABLE Establishments ADD long_wording VARCHAR(255);' +
-      'ALTER TABLE Establishments ADD spinneret VARCHAR(255);' +*/
-      'ALTER TABLE Establishments ADD primary_group_id INT,' +
-      'ADD CONSTRAINT fk_primary_group_id FOREIGN KEY (primary_group_id) REFERENCES Groups(id) ON DELETE SET NULL ON UPDATE CASCADE;';
-      // 'ALTER TABLE Establishments ADD location_updatedAt VARCHAR(255);';
+      'ALTER TABLE Establishments ADD spinneret VARCHAR(255);' +
+      'ALTER TABLE Establishments ADD primary_group_id INT, ADD CONSTRAINT fk_primary_group_id FOREIGN KEY (primary_group_id) REFERENCES `Groups`(id) ON DELETE SET NULL ON UPDATE CASCADE;' +
+      'ALTER TABLE Establishments ADD location_updatedAt VARCHAR(255);' +
+      'COMMIT;';
 
-    sql = 'START TRANSACTION;' + sql + 'COMMIT;';
 
-    return queryInterface.sequelize.query(sql).catch( (err) => {
-      queryInterface.sequelize.query('ROLLBACK;');
-      return Promise.reject('Unexpected error, all changes have been manually rollbacked' + err);
+    /*    return queryInterface.sequelize.query(sql)
+      .then( () => {*/
+
+
+    return new Promise((resolve, reject) => {
+      fs.readFile(`${__}/orm/jsonDatas/croix-rouge.json`, 'utf8', (err, data) => {
+        if (err)
+          return reject(err);
+        try {
+          let datas = JSON.parse(data);
+
+          datas.forEach(d => {
+            let o = {};
+            for (const dKey in d) {
+              if (dKey === 'address1' || dKey === 'address2' || dKey === 'address3' || dKey === 'town1' || dKey === 'town2')
+                continue;
+              if (dKey === 'Adresse physique - Complément'){
+                d.address1 = d[dKey];
+                continue;
+              }
+              o[dKey] = d[dKey];
+            }
+            d.address1 = d.address1 === undefined ? '' : d.address1;
+            d.address2 = d.address2 === undefined ? '' : d.address2;
+            d.address3 = d.address3 === undefined ? '' : d.address3;
+            o.address = d.address1 + ' ' + d.address2 + ' ' + d.address3;
+            o.town = d.town1 + ' ' + d.town2;
+
+            let request = 'INSERT INTO Establishments ' +
+              '(structure_number, attachement_direction, region_code, name, long_wording, spinneret, sector, category, code, url, siret, finess, address, town)' +
+              `VALUES (
+              ${o.structure_number}, 
+              ${o.attachement_direction}, 
+              ${o.region_code}, 
+              ${o.name}, 
+              ${o.long_wording}, 
+              ${o.spinneret}, 
+              ${o.sector}, 
+              ${o.category}, 
+              ${o.code}, ${o.url}, 
+              ${o.siret}, 
+              ${o.finess}, 
+              ${o.address}, 
+              ${o.town})`;
+
+            console.log('SQL : ', request);
+          });
+          resolve (
+            // queryInterface.bulkInsert('Establishments', arrayDatas, { transaction: transaction })
+          );
+        } catch (e) {
+          reject(e);
+        }
+      });
     });
 
 
-    /*.then(() => {
-        /!*return new Promise((resolve, reject) => {
-          fs.readFile(`${__}/orm/jsonDatas/croix-rouge.json`, 'utf8', (err, data) => {
-            if (err)
-              return reject(err);
-            try {
-              let datas = JSON.parse(data);
-              let arrayDatas = [];
-
-              datas.forEach(d => {
-                let formattedObject = {};
-                for (const dKey in d) {
-                  if (dKey === 'address1' || dKey === 'address2' || dKey === 'address3' || dKey === 'town1' || dKey === 'town2')
-                    continue;
-                  if (dKey === 'Adresse physique - Complément'){
-                    d.address1 = d[dKey];
-                    continue;
-                  }
-                  formattedObject[dKey] = d[dKey];
-                }
-                formattedObject.address = d.address1 + ' ' + d.address2 + ' ' + d.address3;
-                formattedObject.town = d.town1 + ' ' + d.town2;
-                arrayDatas.push(formattedObject);
-              });
-              resolve (queryInterface.bulkInsert('Establishments', arrayDatas, { transaction: transaction }));
-            } catch (e) {
-              reject(e);
-            }
-          });
-        });*!/
+    /*  })
+      .catch( (err) => {
+        queryInterface.sequelize.query('ROLLBACK;');
+        return Promise.reject('Unexpected error, all changes have been manually rollbacked' + err);
       });*/
+
+
   },
 
   down: (queryInterface, Sequelize) => {
@@ -82,13 +110,7 @@ module.exports = {
         queryInterface.removeColumn('Establishments', 'long_wording', { transaction: t }),
         queryInterface.removeColumn('Establishments', 'spinneret', { transaction: t }),
         queryInterface.removeColumn('Establishments', 'primary_group_id', { transaction: t }),
-        queryInterface.removeColumn('Establishments', 'location_updatedAt', { transaction: t }),
-        queryInterface.removeColumn('Applications', 'is_available', { transaction: t }),
-        queryInterface.addColumn('Candidates', 'is_available', {
-          type: Sequelize.BOOLEAN,
-          allowNull: true,
-          defaultValue: 1
-        }, { transaction: t }),
+        queryInterface.removeColumn('Establishments', 'location_updatedAt', { transaction: t })
       ])
     })
   }
