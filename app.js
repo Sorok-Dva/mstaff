062380773475a6e62920ce9d867f50d3b2396d82
Сорок два <garcia.llyam@gmail.com>
2019-09-23 16:45:28 +0200

    merge routes declarations into proper file

--- app.js
+++ app.js
@@ -2,18 +2,7 @@ const { Env } = require('./helpers/helpers');
 const { ErrorHandler, Express } = require('./middlewares');
 const path = require('path');
 const express = require('express');
-
-const indexRouter = require('./routes/index');
-const esRouter = require('./routes/es');
-const esSubDomainRouter = require('./routes/esSubdomain');
-const groupSubDomainRouter = require('./routes/groupSubdomain');
-const candidateRouter = require('./routes/candidate');
-const boRouter = require('./routes/backOffice');
-const apiRouter = require('./routes/api/api');
-const apiUserRouter = require('./routes/api/user');
-const apiCandidateRouter = require('./routes/api/candidate');
-const apiBackOfficeRouter = require('./routes/api/backOffice');
-const apiEsRouter = require('./routes/api/establishment');
+const routes = require('./routes/router');
 
 const app = express();
 
@@ -42,25 +31,15 @@ app.use(Express.session);
 app.use(Express.i18n);
 app.use(Express.verifyMaintenance);
 app.use(Express.getServerMessages);
-app.use(Express.passportInit);
-app.use(Express.passportSession);
+app.use(Express.passportInit); // MEMLEAK
+app.use(Express.passportSession); //MEMLEAK
 app.use(Express.flash);
 app.use(Express.setLocals);
 app.use(Express.wildcardSubdomains);
 app.use(Express.readOnlySessionForImpersonation);
 
-// ------ ROUTES
-app.use('/', indexRouter);
-app.use('/', candidateRouter); //candidate
-app.use('/', esRouter); //recruiter
-app.use('/esDomain', esSubDomainRouter);
-app.use('/groupDomain', groupSubDomainRouter);
-app.use('/back-office', boRouter);
-app.use('/api', apiRouter);
-app.use('/api/user', apiUserRouter);
-app.use('/api/candidate', apiCandidateRouter);
-app.use('/api/back-office', apiBackOfficeRouter);
-app.use('/api/es', apiEsRouter);
+// mount all routes on / path
+app.use('/', routes);
 
 if (Env.isProd || Env.isPreProd) app.use(Express.sentryErrorHandler);
 app.use(ErrorHandler.notFoundError);
