7ba04b52d436e2ba180de052fec254bd090f45bf
Leo450 <macaque57>
2019-09-25 15:00:39 +0200

    <3 Romain fix

--- orm/models/establishment.js
+++ orm/models/establishment.js
@@ -93,7 +93,7 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: 'primary_group_id',
       targetKey: 'id'
     });
-    Establishment.hasOne(models.EstablishmentCategories, {
+    Establishment.hasOne(models.EstablishmentTypes, {
       foreignKey: 'id'
     })
   };
